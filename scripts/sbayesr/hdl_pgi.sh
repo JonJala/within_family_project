7fed10d304ce267ea8a51f8b5ab723d5f3b9837f
Tammy Tan <tammy.t133@gmail.com>
2022-04-22 15:40:10 -0400

    script to calculate hdl pgis

---
+++ scripts/sbayesr/hdl_pgi.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# Reference sbayesr code borrowed from Aysu
+# Original code /disk/genetics4/projects/EA4/code/PGS/7_PGS_SBayesR.sh
+
+within_family_path="/var/genetics/proj/within_family/within_family_project"
+
+source /var/genetics/proj/within_family/within_family_project/scripts/sbayesr/sbayesrfunc.sh
+
+# ============= Execution ============= #
+run_pgi "${within_family_path}/processed/package_output/bmi/meta.sumstats.gz" "direct" "hdl" "ukb"
+# run_pgi "${within_family_path}/processed/package_output/bmi/meta.sumstats.gz" "population" "hdl" "ukb"
+

6ec0cf40be3b44dda7715da95f0e0360059bc076
Hariharan Jayashankar <hariharan.jayashankar@gmail.com>
2022-04-24 18:51:27 -0400

    wrote logistic reg func for fpgs

--- scripts/fpgs/fpgs_reg.py
+++ scripts/fpgs/fpgs_reg.py
@@ -14,8 +14,7 @@ def run_fpgs_reg(args):
     runstr = f'''
 python {args.outpath}\
 --pgs {args.pgs} \
---phenofile {args.phenofile} \
---pgsreg-r2 
+--phenofile {args.phenofile}
 '''
 
     if args.pgsreg_r2:
@@ -29,10 +28,26 @@ def run_logistic_reg(args):
     pgs = pd.read_csv(args.pgs, delim_whitespace=True)
     phenofile = pd.read_csv(args.phenofile, delim_whitespace=True, names = ['FID', 'IID', 'pheno'])
     regvars = [c for c in pgs.columns if c not in ['FID', 'IID']]
+    regvars = [c for c in regvars if c not in ['age2', 'age3', 'agesex', 'age2sex', 'age3sex']]
 
     dat = pd.merge(pgs, phenofile, on = 'IID', how='inner')
 
     logreg = smf.logit('pheno~' + '+'.join(regvars), data=dat).fit()
+
+    ests = logreg.params[1:]
+
+    ses = logreg.bse[1:]
+    r2 = logreg.prsquared
+
+    outdat = pd.DataFrame(
+        {
+            'ests' : ests,
+            'ses' : ses,
+            'r2' : r2
+        }
+    )
+
+    outdat.to_csv(args.outpath, sep = ' ', header = False)
 
 
 
@@ -48,5 +63,9 @@ if __name__ == '__main__':
     parser.add_argument('--logistic', type=bool, action='store_true', help="Should regression be a logistic regression")
     args = parser.parse_args()
 
+    if not args.logistic:
+        run_fpgs_reg(args)
+    else:
+        run_logistic_reg(args)
 
 

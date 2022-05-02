c08e811f033f447f491b3fc478281adf7d5f394f
Tammy Tan <tammy.t133@gmail.com>
2022-05-02 10:30:01 -0400

    use ukb not mcs

--- scripts/sbayesr/cognition_pgi.sh
+++ scripts/sbayesr/cognition_pgi.sh
@@ -1,4 +1,4 @@
-#!usr/bin/bash
+#!/usr/bin/bash
 
 # Reference sbayesr code borrowed from Aysu
 # Original code /disk/genetics4/projects/EA4/code/PGS/7_PGS_SBayesR.sh
@@ -9,5 +9,8 @@ within_family_path="/var/genetics/proj/within_family/within_family_project"
 source /var/genetics/proj/within_family/within_family_project/scripts/sbayesr/sbayesrfunc.sh
 
 # ============= Execution ============= #
-run_pgi "${within_family_path}/processed/package_output/cog/meta.sumstats.gz" "direct" "cognition" "mcs"
-run_pgi "${within_family_path}/processed/package_output/cog/meta.sumstats.gz" "population" "cognition" "mcs"
+# run_pgi "${within_family_path}/processed/package_output/cognition/meta.sumstats.gz" "direct" "cognition" "mcs"
+# run_pgi "${within_family_path}/processed/package_output/cognition/meta.sumstats.gz" "population" "cognition" "mcs"
+
+run_pgi "${within_family_path}/processed/package_output/cognition/meta.sumstats.gz" "direct" "cognition" "ukb"
+run_pgi "${within_family_path}/processed/package_output/cognition/meta.sumstats.gz" "population" "cognition" "ukb"

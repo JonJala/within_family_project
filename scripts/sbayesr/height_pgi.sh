fa5a07161949e28a9640337c498c57dda6f9891d
Tammy Tan <tammy.t133@gmail.com>
2022-05-02 11:17:01 -0400

    specify mcs

--- scripts/sbayesr/height_pgi.sh
+++ scripts/sbayesr/height_pgi.sh
@@ -8,6 +8,6 @@ within_family_path="/var/genetics/proj/within_family/within_family_project"
 source /var/genetics/proj/within_family/within_family_project/scripts/sbayesr/sbayesrfunc.sh
 
 # ============= Execution ============= #
-run_pgi "${within_family_path}/processed/package_output/height/meta.sumstats.gz" "direct" "height"
-run_pgi "${within_family_path}/processed/package_output/height/meta.sumstats.gz" "population" "height"
+run_pgi "${within_family_path}/processed/package_output/height/meta.sumstats.gz" "direct" "height" "mcs"
+run_pgi "${within_family_path}/processed/package_output/height/meta.sumstats.gz" "population" "height" "mcs"
 

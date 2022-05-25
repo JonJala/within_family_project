4e64fb941a60605b61130ee5151d59ea305a248e
Tammy Tan <tammy.t133@gmail.com>
2022-05-25 16:48:42 -0400

    uncomment some stuff

--- scripts/usingpackage/ea/runmeta.sh
+++ scripts/usingpackage/ea/runmeta.sh
@@ -2,19 +2,19 @@
 
 scriptpath="/var/genetics/proj/within_family/within_family_project/scripts/package"
 
-# # Base
-# python $scriptpath/run_metaanalysis.py \
-# "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/ea/inputfiles.json" \
-# --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/ea/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/ea/meta.log 
+# Base
+python $scriptpath/run_metaanalysis.py \
+"/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/ea/inputfiles.json" \
+--outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/ea/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/ea/meta.log 
 # # with ea4
 # python $scriptpath/run_metaanalysis.py \
 # "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/ea/inputfiles_withea4.json" \
 # --outestimates "direct_population" \
 # --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/ea/ea_meta_analysis_ea4"
 
-# without ukb
-python $scriptpath/run_metaanalysis.py \
-"/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/ea/inputfiles_noukb.json" \
---outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/ea/meta_noukb"
+# # without ukb
+# python $scriptpath/run_metaanalysis.py \
+# "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/ea/inputfiles_noukb.json" \
+# --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/ea/meta_noukb"
 
 

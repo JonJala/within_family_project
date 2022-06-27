7250945296c4e64ceb2924f509811c9527b87ee7
Tammy Tan <tammy.t133@gmail.com>
2022-06-27 11:45:13 -0400

    save number of snps in sumstats files

--- scripts/sbayesr/ea4_meta/process_ea_sumstats.r
+++ scripts/sbayesr/ea4_meta/process_ea_sumstats.r
@@ -10,7 +10,7 @@ library(stringr)
 # define function
 # ---------------------------------------------------------------------------------------------
 
-process_sumstats <- function(ss, no_ukb, lead_snps_only = FALSE) {
+process_sumstats <- function(ss, no_ukb, lead_snps_only = FALSE, save = TRUE) {
 
     if (lead_snps_only == TRUE) {
 
@@ -45,6 +45,10 @@ process_sumstats <- function(ss, no_ukb, lead_snps_only = FALSE) {
     print(paste0("There are ", length(unique(ea4_snps$rsID)), " genome-wide significant SNPs in the EA4 sumstats."))
     print(paste0("There are ", sum(unique(ea4_snps$rsID) %in% unique(ss$SNP)), " SNPs in the overlap between the sumstats files."))
 
+    n_snps <- data.frame("ea4_snps" = length(unique(ea4_snps$rsID)), "snps_overlap" = sum(unique(ea4_snps$rsID) %in% unique(ss$SNP)))
+    path <- ifelse(no_ukb == TRUE, "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/weights/n_snps_ukb", "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/weights/n_snps_mcs")
+    fwrite(n_snps, path, quote = F, sep = "\t")
+
     # get direct effects for these SNPs
     ea_final <- ss %>% 
                     filter(SNP %in% ea4_snps$rsID) %>%
@@ -55,13 +59,15 @@ process_sumstats <- function(ss, no_ukb, lead_snps_only = FALSE) {
         mutate(ID = seq(1,nrow(ea_final)), varid = paste0(Chr, ":", BP)) %>%
         relocate(c("ID", "SNP", "direct_N", "direct_SE", "A1", "A2", "freq", "direct_Beta", "direct_pval", "Chr", "BP", "varid"))
 
-    # save
-    if (no_ukb == FALSE) {
-        fwrite(ea_final, "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/weights/meta_weights.snpRes.formatted", quote = F, sep = "\t")    
-    } else {
-        fwrite(ea_final, "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/weights/meta_noukb_weights.snpRes.formatted", quote = F, sep = "\t")
+    if (save == TRUE) {
+        # save
+        if (no_ukb == FALSE) {
+            fwrite(ea_final, "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/weights/meta_weights.snpRes.formatted", quote = F, sep = "\t")    
+        } else {
+            fwrite(ea_final, "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/weights/meta_noukb_weights.snpRes.formatted", quote = F, sep = "\t")
+        }
     }
-
+    
     # plot distribution of no. cohorts
     title <- ifelse(no_ukb == FALSE, "Number of Cohorts Contributing to Meta-Analysis", "Number of Cohorts Contributing to Meta-Analysis (Without UKB)")
     p <- ggplot(ss[ss$SNP %in% ea4_snps$rsID],
@@ -73,9 +79,11 @@ process_sumstats <- function(ss, no_ukb, lead_snps_only = FALSE) {
                 theme_classic()
     print(p)
 
-    # save plot
-    path <- ifelse(no_ukb == FALSE, "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/n_cohorts.png", "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/n_cohorts_noukb.png")
-    ggsave(filename = path)
+    if (save == TRUE) {
+        # save plot
+        path <- ifelse(no_ukb == FALSE, "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/n_cohorts.png", "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/n_cohorts_noukb.png")
+        ggsave(filename = path)
+    }
 
 }
 

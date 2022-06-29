0d2c188ef42c3ecbff2d9f2cc5109c217a504839
Tammy Tan <tammy.t133@gmail.com>
2022-06-29 01:39:48 -0400

    use snps with largest direct N in our ss

--- scripts/sbayesr/ea4_meta/process_ea_sumstats.r
+++ scripts/sbayesr/ea4_meta/process_ea_sumstats.r
@@ -18,46 +18,42 @@ process_sumstats <- function(ss, no_ukb, lead_snps_only = FALSE, save = TRUE) {
         ea4_snps <- fread("/var/genetics/proj/within_family/within_family_project/processed/ea4_meta/EA4_additive_p1e-5_clumped.txt")
 
         # filter p-val < 5*10^-8
-        ea4_snps %<>% filter(P < 5*10^-8)
+        ea4_snps %<>% filter(P < 5*10^-8) %>% rename(SNP = "rsID")
+
+            # get direct effects for these SNPs
+        ea_final <- ss %>% filter(SNP %in% ea4_snps$SNP)
         
     } else {
 
-        ## all GWS SNPs from EA4
-        
-        full_ea4 <- fread("/disk/genetics4/projects/EA4/derived_data/Meta_analysis/1_Main/2020_08_20/output/EA4_2020_08_20.meta") # full ea4 metaanalysed ss
-        
-        # get list of all gws snps
-        snplist <- c()
+        # get list of all gws snps from each ea4 clump
+        ea4_snps <- data.table()
         for (chr in 1:22) {
-            clump <- fread(paste0("/disk/genetics/ukb/aokbay/EA4_clumped/EA4_2020_08_20.meta.chr", chr, ".clumped"))
-            lead_snps <- clump$SNP
-            other_snps <- unlist(strsplit(clump$SP2, "\\(1\\),"))
-            snplist <- append(snplist, lead_snps)
-            snplist <- append(snplist, other_snps)
+            clumps <- fread(paste0("/disk/genetics/ukb/aokbay/EA4_clumped/EA4_2020_08_20.meta.chr", chr, ".clumped"))
+            for (row in 1:nrow(clumps)) {
+                snplist <- c()
+                lead_snp <- clumps$SNP[row]
+                other_snps <- unlist(strsplit(clumps$SP2[row], "\\(1\\),"))
+                snplist <- append(snplist, lead_snp)
+                snplist <- append(snplist, other_snps)
+                snplist <- str_remove_all(snplist, "\\(1\\)") # remove any remaining instances of (1)
+                snplist <- snplist[! snplist %in% "NONE"] # remove all instances of "NONE"
+                ea4_snps <- rbind(ea4_snps, data.table(SNP = snplist, Chr = chr, clump_number = row))
+            }
         }
-        snplist <- str_remove_all(snplist, "\\(1\\)") # remove any remaining instances of (1)
 
-        # select these snps from ea4 sumstats
-        ea4_snps <- full_ea4 %>% 
-                        filter(rsID %in% snplist)
+        ea_final <- ss %>%
+                    filter(SNP %in% ea4_snps$SNP) %>%
+                    left_join(ea4_snps) %>%
+                    group_by(chromosome, clump_number) %>%
+                    filter(direct_N == max(direct_N)) %>%
+                    ungroup()
+
     }
-    
-    print(paste0("There are ", length(unique(ea4_snps$rsID)), " genome-wide significant SNPs in the EA4 sumstats."))
-    print(paste0("There are ", sum(unique(ea4_snps$rsID) %in% unique(ss$SNP)), " SNPs in the overlap between the sumstats files."))
-
-    n_snps <- data.frame("ea4_snps" = length(unique(ea4_snps$rsID)), "snps_overlap" = sum(unique(ea4_snps$rsID) %in% unique(ss$SNP)))
-    path <- ifelse(no_ukb == TRUE, "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/weights/n_snps_ukb", "/var/genetics/proj/within_family/within_family_project/processed/sbayesr/ea4_meta/direct/weights/n_snps_mcs")
-    fwrite(n_snps, path, quote = F, sep = "\t")
-
-    # get direct effects for these SNPs
-    ea_final <- ss %>% 
-                    filter(SNP %in% ea4_snps$rsID) %>%
-                    merge(ea4_snps[,c("rsID", "Chr", "BP")], by.x = "SNP", by.y = "rsID")
 
     # format in plink format
     ea_final %<>% 
-        mutate(ID = seq(1,nrow(ea_final)), varid = paste0(Chr, ":", BP)) %>%
-        relocate(c("ID", "SNP", "direct_N", "direct_SE", "A1", "A2", "freq", "direct_Beta", "direct_pval", "Chr", "BP", "varid"))
+        mutate(ID = seq(1,nrow(ea_final)), varid = cptid, Pos = pos) %>%
+        select("ID", "SNP", "direct_N", "direct_SE", "A1", "A2", "freq", "direct_Beta", "direct_pval", "Chr", "Pos", "varid")
 
     if (save == TRUE) {
         # save
@@ -70,7 +66,7 @@ process_sumstats <- function(ss, no_ukb, lead_snps_only = FALSE, save = TRUE) {
     
     # plot distribution of no. cohorts
     title <- ifelse(no_ukb == FALSE, "Number of Cohorts Contributing to Meta-Analysis", "Number of Cohorts Contributing to Meta-Analysis (Without UKB)")
-    p <- ggplot(ss[ss$SNP %in% ea4_snps$rsID],
+    p <- ggplot(ss[ss$SNP %in% ea4_snps$SNP],
                 aes(x = n_cohorts)) +
                 geom_histogram(stat = "count", fill = "lightskyblue3") +
                 labs(x="Number of Cohorts", y="Count") +

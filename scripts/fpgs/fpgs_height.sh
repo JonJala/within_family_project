4c743036959e1e622ab4657af082b7debe2fa40b
Tammy Tan <tammy.t133@gmail.com>
2022-08-04 16:30:48 -0400

    remove sniparpath

--- scripts/fpgs/fpgs_height.sh
+++ scripts/fpgs/fpgs_height.sh
@@ -2,7 +2,6 @@
 source /var/genetics/proj/within_family/within_family_project/snipar/bin/activate
 
 within_family_path="/var/genetics/proj/within_family/within_family_project"
-snipar_path="/homes/nber/harij/gitrepos/SNIPar"
 bedfilepath="/var/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/bgen/tmp/chr~.dose"
 impfilespath="/var/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/imputed_parents/chr~"
 phenofile="/var/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/phen/phenotypes.txt"

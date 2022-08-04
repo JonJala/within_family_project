21b63b03e4417af38f303eaa8d668a6412a8df42
Tammy Tan <tammy.t133@gmail.com>
2022-08-04 16:24:22 -0400

    update ldsc analysis files

---
+++ scripts/usingpackage/agemenarche/ldsc_analysis.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+
+ldscpath="/var/genetics/pub/software/ldsc"
+ldscmodpath="/var/genetics/proj/within_family/within_family_project/ssgac/ldsc_mod"
+eur_w_ld_chr="/var/genetics/pub/data/ld_ref_panel/eur_w_ld_chr/"
+within_family_path="/var/genetics/proj/within_family/within_family_project"
+hm3snps="/disk/genetics2/pub/data/PH3_Reference/w_hm3.snplist"
+scriptpath="/var/genetics/proj/within_family/within_family_project/scripts/package"
+
+source /disk/genetics/pub/python_env/anaconda2/bin/activate /disk/genetics/pub/python_env/anaconda2/envs/ldsc
+
+echo "Munging!!"
+${ldscpath}/munge_sumstats.py \
+--sumstats ${within_family_path}/processed/package_output/agemenarche/meta.nfilter.sumstats.gz \
+--out ${within_family_path}/processed/package_output/agemenarche/populationmunged \
+--N-col population_N --p population_pval --signed-sumstats population_z,0 \
+--merge-alleles ${hm3snps} \
+--n-min 1.0
+
+${ldscpath}/munge_sumstats.py \
+--sumstats ${within_family_path}/processed/package_output/agemenarche/meta.nfilter.sumstats.gz \
+--out ${within_family_path}/processed/package_output/agemenarche/directmunged \
+--N-col direct_N --p direct_pval --signed-sumstats direct_z,0 \
+--merge-alleles ${hm3snps} \
+--n-min 1.0
+
+echo "Calcualting RG of population effect with reference sample"
+${ldscpath}/ldsc.py \
+--rg ${within_family_path}/processed/package_output/agemenarche/populationmunged.sumstats.gz,${within_family_path}/processed/reference_samples/ht_ref/Meta-analysis_Wood_et_al+UKBiobank_2018.sumstats.gz \
+--ref-ld-chr ${eur_w_ld_chr} \
+--w-ld-chr ${eur_w_ld_chr} \
+--out ${within_family_path}/processed/package_output/agemenarche/population_reference_sample
+# 0.901 (0.0059)
+
+echo "Calcualting RG of direct effect with reference sample"
+${ldscpath}/ldsc.py \
+--rg ${within_family_path}/processed/package_output/agemenarche/directmunged.sumstats.gz,${within_family_path}/processed/reference_samples/ht_ref/Meta-analysis_Wood_et_al+UKBiobank_2018.sumstats.gz \
+--ref-ld-chr ${eur_w_ld_chr} \
+--w-ld-chr ${eur_w_ld_chr} \
+--out ${within_family_path}/processed/package_output/agemenarche/direct_reference_sample
+# 0.901 (0.0059)
+
+
+${ldscpath}/ldsc.py \
+--h2 ${within_family_path}/processed/package_output/agemenarche/directmunged.sumstats.gz \
+--ref-ld-chr ${eur_w_ld_chr} \
+--w-ld-chr ${eur_w_ld_chr} \
+--out ${within_family_path}/processed/package_output/agemenarche/direct_h2
+#  0.3274 (0.0175)
+
+${ldscpath}/ldsc.py \
+--h2 ${within_family_path}/processed/package_output/agemenarche/populationmunged.sumstats.gz \
+--ref-ld-chr ${eur_w_ld_chr} \
+--w-ld-chr ${eur_w_ld_chr} \
+--out ${within_family_path}/processed/package_output/agemenarche/population_h2
+#  0.3274 (0.0175)
+
+
+# Changing env
+source /var/genetics/proj/within_family/within_family_project/snipar/bin/activate
+/var/genetics/proj/within_family/within_family_project/snipar/snipar/scripts/correlate.py /var/genetics/proj/within_family/within_family_project/processed/package_output/agemenarche/meta.nfilter \
+/var/genetics/proj/within_family/within_family_project/processed/package_output/agemenarche/marginal \
+--ldscores /disk/genetics/ukb/alextisyoung/hapmap3/ldscores/@
--- scripts/usingpackage/neuroticism/ldsc_analysis.sh
+++ scripts/usingpackage/neuroticism/ldsc_analysis.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 ldscpath="/var/genetics/pub/software/ldsc"
-ldscmodpath="/homes/nber/harij/ssgac/ldsc_mod"
+ldscmodpath="/var/genetics/proj/within_family/within_family_project/ssgac/ldsc_mod"
 eur_w_ld_chr="/var/genetics/pub/data/ld_ref_panel/eur_w_ld_chr/"
 within_family_path="/var/genetics/proj/within_family/within_family_project"
 hm3snps="/disk/genetics2/pub/data/PH3_Reference/w_hm3.snplist"
@@ -11,53 +11,53 @@ source /disk/genetics/pub/python_env/anaconda2/bin/activate /disk/genetics/pub/p
 
 echo "Munging!!"
 ${ldscpath}/munge_sumstats.py \
---sumstats ${within_family_path}/processed/package_output/height/meta.nfilter.sumstats.gz \
---out ${within_family_path}/processed/package_output/height/populationmunged \
+--sumstats ${within_family_path}/processed/package_output/neuroticism/meta.nfilter.sumstats.gz \
+--out ${within_family_path}/processed/package_output/neuroticism/populationmunged \
 --N-col population_N --p population_pval --signed-sumstats population_z,0 \
 --merge-alleles ${hm3snps} \
 --n-min 1.0
 
 ${ldscpath}/munge_sumstats.py \
---sumstats ${within_family_path}/processed/package_output/height/meta.nfilter.sumstats.gz \
---out ${within_family_path}/processed/package_output/height/directmunged \
+--sumstats ${within_family_path}/processed/package_output/neuroticism/meta.nfilter.sumstats.gz \
+--out ${within_family_path}/processed/package_output/neuroticism/directmunged \
 --N-col direct_N --p direct_pval --signed-sumstats direct_z,0 \
 --merge-alleles ${hm3snps} \
 --n-min 1.0
 
 echo "Calcualting RG of population effect with reference sample"
 ${ldscpath}/ldsc.py \
---rg ${within_family_path}/processed/package_output/height/populationmunged.sumstats.gz,${within_family_path}/processed/reference_samples/ht_ref/Meta-analysis_Wood_et_al+UKBiobank_2018.sumstats.gz \
+--rg ${within_family_path}/processed/package_output/neuroticism/populationmunged.sumstats.gz,${within_family_path}/processed/reference_samples/ht_ref/Meta-analysis_Wood_et_al+UKBiobank_2018.sumstats.gz \
 --ref-ld-chr ${eur_w_ld_chr} \
 --w-ld-chr ${eur_w_ld_chr} \
---out ${within_family_path}/processed/package_output/height/population_reference_sample
+--out ${within_family_path}/processed/package_output/neuroticism/population_reference_sample
 # 0.901 (0.0059)
 
 echo "Calcualting RG of direct effect with reference sample"
 ${ldscpath}/ldsc.py \
---rg ${within_family_path}/processed/package_output/height/directmunged.sumstats.gz,${within_family_path}/processed/reference_samples/ht_ref/Meta-analysis_Wood_et_al+UKBiobank_2018.sumstats.gz \
+--rg ${within_family_path}/processed/package_output/neuroticism/directmunged.sumstats.gz,${within_family_path}/processed/reference_samples/ht_ref/Meta-analysis_Wood_et_al+UKBiobank_2018.sumstats.gz \
 --ref-ld-chr ${eur_w_ld_chr} \
 --w-ld-chr ${eur_w_ld_chr} \
---out ${within_family_path}/processed/package_output/height/direct_reference_sample
+--out ${within_family_path}/processed/package_output/neuroticism/direct_reference_sample
 # 0.901 (0.0059)
 
 
 ${ldscpath}/ldsc.py \
---h2 ${within_family_path}/processed/package_output/height/directmunged.sumstats.gz \
+--h2 ${within_family_path}/processed/package_output/neuroticism/directmunged.sumstats.gz \
 --ref-ld-chr ${eur_w_ld_chr} \
 --w-ld-chr ${eur_w_ld_chr} \
---out ${within_family_path}/processed/package_output/height/direct_h2
+--out ${within_family_path}/processed/package_output/neuroticism/direct_h2
 #  0.3274 (0.0175)
 
 ${ldscpath}/ldsc.py \
---h2 ${within_family_path}/processed/package_output/height/populationmunged.sumstats.gz \
+--h2 ${within_family_path}/processed/package_output/neuroticism/populationmunged.sumstats.gz \
 --ref-ld-chr ${eur_w_ld_chr} \
 --w-ld-chr ${eur_w_ld_chr} \
---out ${within_family_path}/processed/package_output/height/population_h2
+--out ${within_family_path}/processed/package_output/neuroticism/population_h2
 #  0.3274 (0.0175)
 
 
 # Changing env
-source /disk/genetics/pub/python_env/anaconda2/bin/activate /var/genetics/proj/within_family/within_family_project/sniparenv
-/var/genetics/proj/within_family/within_family_project/snipar/snipar/scripts/correlate.py /var/genetics/proj/within_family/within_family_project/processed/package_output/height/meta.nfilter \
-/var/genetics/proj/within_family/within_family_project/processed/package_output/height/marginal \
+source /var/genetics/proj/within_family/within_family_project/snipar/bin/activate
+/var/genetics/proj/within_family/within_family_project/snipar/snipar/scripts/correlate.py /var/genetics/proj/within_family/within_family_project/processed/package_output/neuroticism/meta.nfilter \
+/var/genetics/proj/within_family/within_family_project/processed/package_output/neuroticism/marginal \
 --ldscores /disk/genetics/ukb/alextisyoung/hapmap3/ldscores/@

a2c221d40060985302565dfdd47663ce62bc28e8
Tammy Tan <tammy.t133@gmail.com>
2022-08-05 14:07:13 -0400

    metaanalysis with ukb included

--- scripts/usingpackage/cognition/runmeta.sh
+++ scripts/usingpackage/cognition/runmeta.sh
@@ -3,13 +3,13 @@
 scriptpath="/var/genetics/proj/within_family/within_family_project/scripts/package"
 
 
-# python $scriptpath/run_metaanalysis.py \
-# "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/cognition/inputfiles.json" \
-# --outestimates "avgparental_to_population" \
-# --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/cognition/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/cognition/meta.log
-
-
 python $scriptpath/run_metaanalysis.py \
-"/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/cognition/inputfiles_noukb.json" \
+"/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/cognition/inputfiles.json" \
 --outestimates "avgparental_to_population" \
---outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/cognition/meta_noukb" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/cognition/meta_noukb.log
+--outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/cognition/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/cognition/meta.log
+
+
+# python $scriptpath/run_metaanalysis.py \
+# "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/cognition/inputfiles_noukb.json" \
+# --outestimates "avgparental_to_population" \
+# --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/cognition/meta_noukb" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/cognition/meta_noukb.log

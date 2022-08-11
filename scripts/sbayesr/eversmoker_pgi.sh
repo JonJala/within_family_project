fc56b5bfe68f86f39d7399fee21c61903fde484c
Tammy Tan <tammy.t133@gmail.com>
2022-08-11 16:34:48 -0400

    uncomment

--- scripts/sbayesr/eversmoker_pgi.sh
+++ scripts/sbayesr/eversmoker_pgi.sh
@@ -1,4 +1,4 @@
-#!usr/bin/bash
+#!/usr/bin/bash
 
 # Reference sbayesr code borrowed from Aysu
 # Original code /disk/genetics4/projects/EA4/code/PGS/7_PGS_SBayesR.sh
@@ -8,12 +8,12 @@ within_family_path="/var/genetics/proj/within_family/within_family_project"
 source /var/genetics/proj/within_family/within_family_project/scripts/sbayesr/sbayesrfunc.sh
 
 # ============= Execution ============= #
-# run_pgi "${within_family_path}/processed/package_output/eversmoker/meta.sumstats.gz" "direct" "eversmoker" "mcs"
-# run_pgi "${within_family_path}/processed/package_output/eversmoker/meta.sumstats.gz" "population" "eversmoker" "mcs"
+run_pgi "${within_family_path}/processed/package_output/eversmoker/meta.sumstats.gz" "direct" "eversmoker" "mcs"
+run_pgi "${within_family_path}/processed/package_output/eversmoker/meta.sumstats.gz" "population" "eversmoker" "mcs"
 
 ######################
 # clumping analysis
 #####################
 
-run_pgi "${within_family_path}/processed/clumping_analysis/eversmoker/direct/weights/mcs/meta_weights.snpRes.formatted" "direct" "eversmoker" "mcs" "clump"
-run_pgi "${within_family_path}/processed/clumping_analysis/eversmoker/population/weights/mcs/meta_weights.snpRes.formatted" "population" "eversmoker" "mcs" "clump"
+# run_pgi "${within_family_path}/processed/clumping_analysis/eversmoker/direct/weights/mcs/meta_weights.snpRes.formatted" "direct" "eversmoker" "mcs" "clump"
+# run_pgi "${within_family_path}/processed/clumping_analysis/eversmoker/population/weights/mcs/meta_weights.snpRes.formatted" "population" "eversmoker" "mcs" "clump"
--- scripts/sbayesr/hdl_pgi.sh
+++ scripts/sbayesr/hdl_pgi.sh
@@ -8,12 +8,12 @@ within_family_path="/var/genetics/proj/within_family/within_family_project"
 source /var/genetics/proj/within_family/within_family_project/scripts/sbayesr/sbayesrfunc.sh
 
 # ============= Execution ============= #
-# run_pgi "${within_family_path}/processed/package_output/hdl/meta.sumstats.gz" "direct" "hdl" "ukb"
-# run_pgi "${within_family_path}/processed/package_output/hdl/meta.sumstats.gz" "population" "hdl" "ukb"
+run_pgi "${within_family_path}/processed/package_output/hdl/meta.sumstats.gz" "direct" "hdl" "ukb"
+run_pgi "${within_family_path}/processed/package_output/hdl/meta.sumstats.gz" "population" "hdl" "ukb"
 
 ######################
 # clumping analysis
 #####################
 
-run_pgi "${within_family_path}/processed/clumping_analysis/hdl/direct/weights/ukb/meta_weights.snpRes.formatted" "direct" "hdl" "ukb" "clump"
-run_pgi "${within_family_path}/processed/clumping_analysis/hdl/population/weights/ukb/meta_weights.snpRes.formatted" "population" "hdl" "ukb" "clump"
+# run_pgi "${within_family_path}/processed/clumping_analysis/hdl/direct/weights/ukb/meta_weights.snpRes.formatted" "direct" "hdl" "ukb" "clump"
+# run_pgi "${within_family_path}/processed/clumping_analysis/hdl/population/weights/ukb/meta_weights.snpRes.formatted" "population" "hdl" "ukb" "clump"

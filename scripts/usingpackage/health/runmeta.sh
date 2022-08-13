c56266cb3b0c0921d6e3cb9ec0f0d6cccd04b3f0
Tammy Tan <tammy.t133@gmail.com>
2022-08-12 20:17:49 -0400

    rename srh to health

---
+++ scripts/usingpackage/health/runmeta.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+scriptpath="/var/genetics/proj/within_family/within_family_project/scripts/package"
+
+python $scriptpath/run_metaanalysis.py \
+"/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/health/inputfiles.json" \
+--outestimates "avgparental_to_population" \
+--outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/health/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/health/meta.log
+
--- scripts/usingpackage/srh/runmeta.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-scriptpath="/var/genetics/proj/within_family/within_family_project/scripts/package"
-
-python $scriptpath/run_metaanalysis.py \
-"/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/srh/inputfiles.json" \
---outestimates "avgparental_to_population" \
---outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/srh/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/srh/meta.log
-

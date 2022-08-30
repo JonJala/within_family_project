d35acb7ad70769c80896a9676a7afeb9a06dd700
Tammy Tan <tammy.t133@gmail.com>
2022-08-30 14:36:43 -0400

    .

--- scripts/usingpackage/ea/runmeta.sh
+++ scripts/usingpackage/ea/runmeta.sh
@@ -6,6 +6,7 @@ scriptpath="/var/genetics/proj/within_family/within_family_project/scripts/packa
 python $scriptpath/run_metaanalysis.py \
 "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/ea/inputfiles.json" \
 --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/ea/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/ea/meta.log 
+
 # # with ea4
 # python $scriptpath/run_metaanalysis.py \
 # "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/ea/inputfiles_withea4.json" \

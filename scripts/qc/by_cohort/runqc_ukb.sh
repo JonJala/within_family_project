3fd6229804c30a3fc817ea95ba3f5984300ca721
Tammy Tan <tammy.t133@gmail.com>
2022-09-06 13:01:56 -0400

    script for ukb phenos qc

--- scripts/qc/by_cohort/runqc_ukb.sh
+++ scripts/qc/by_cohort/runqc_ukb.sh
@@ -3,12 +3,15 @@
 # run qc for all ukb phenos
 # note: make sure to comment out all the other cohorts' code from each file first!!
 
-# /var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_height.sh
-/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_neuroticism.sh
-/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_hdl.sh
-/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_fev.sh
-/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_eversmoker.sh
-/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_ea.sh
-/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_cognition.sh
-/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_bmi.sh
-/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_aafb.sh
+/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_height.sh &
+/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_neuroticism.sh &
+/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_hdl.sh &
+/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_fev.sh &
+wait
+
+/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_eversmoker.sh &
+/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_ea.sh &
+/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_cognition.sh &
+/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_bmi.sh &
+/var/genetics/proj/within_family/within_family_project/scripts/qc/runqc_aafb.sh &
+wait

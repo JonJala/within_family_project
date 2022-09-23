5e8100c64817ca55827504f9279fc59d5b393b36
Tammy Tan <tammy.t133@gmail.com>
2022-09-23 18:05:10 -0400

    fix path

--- scripts/qc/runqc_asthma.sh
+++ scripts/qc/runqc_asthma.sh
@@ -58,7 +58,7 @@ reffile="/var/genetics/proj/within_family/within_family_project/processed/refere
 ############
 
 python /var/genetics/proj/within_family/within_family_project/scripts/package/qc/run_easyqc.py \
-    "/disk/genetics3/data_dirs/ipsych/private/v1/processed/sumstats/stats/sumstats_asthma_export.txt" \
+    "/disk/genetics3/data_dirs/ipsych/private/v1/processed/sumstats/sumstats_asthma_export.txt" \
     --outprefix "/var/genetics/proj/within_family/within_family_project/processed/qc/ipsych/asthma" \
     --toest "direct_population" \
     --ldsc-ref "$reffile"

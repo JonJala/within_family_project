385169abe3a2607fa0f317655f3a96a06f199709
Tammy Tan <tammy.t133@gmail.com>
2022-09-26 19:09:37 -0400

    script to make ipsych conform to QC colnames

---
+++ scripts/qc/clean_ipsych.py
@@ -0,0 +1,27 @@
+'''
+Rename iPSYCH sumstats columns so that they align with QC pipeline
+'''
+
+import pandas as pd
+import glob
+
+files = glob.glob("/disk/genetics3/data_dirs/ipsych/private/v1/raw/sumstats/sumstats_*")
+
+for file in files:
+
+    print(file)
+
+    ss = pd.read_csv(file, sep = "\t")
+    ss.columns = ss.columns.str.replace("_beta", "_Beta")
+    ss.columns = ss.columns.str.replace("_ntc", "_NTC")
+    ss.columns = ss.columns.str.replace("_n", "_N")
+    ss.columns = ss.columns.str.replace("_se", "_SE")
+    ss.columns = ss.columns.str.replace("_z", "_Z")
+    ss.columns = ss.columns.str.replace("a1", "A1")
+    ss.columns = ss.columns.str.replace("a2", "A2")
+    ss.columns = ss.columns.str.replace("snp", "SNP")
+
+    savepath = file.replace("raw", "processed")
+    ss.to_csv(savepath, sep = " ", index = False, na_rep = "NA")
+
+    print(f"Done with {file}")

b406f4791d522774542ad96507661937798c1fdb
Tammy Tan <tammy.t133@gmail.com>
2022-12-07 17:15:20 -0500

    switch to hm3

--- scripts/sbayesr/eversmoker_pgi.sh
+++ scripts/sbayesr/eversmoker_pgi.sh
@@ -8,8 +8,8 @@ within_family_path="/var/genetics/proj/within_family/within_family_project"
 source /var/genetics/proj/within_family/within_family_project/scripts/sbayesr/sbayesrfunc.sh
 
 # ============= Execution ============= #
-run_pgi "${within_family_path}/processed/package_output/eversmoker/meta.sumstats.gz" "direct" "eversmoker" "mcs"
-run_pgi "${within_family_path}/processed/package_output/eversmoker/meta.sumstats.gz" "population" "eversmoker" "mcs"
+run_pgi "${within_family_path}/processed/package_output/eversmoker/meta.hm3.sumstats.gz" "direct" "eversmoker" "mcs"
+run_pgi "${within_family_path}/processed/package_output/eversmoker/meta.hm3.sumstats.gz" "population" "eversmoker" "mcs"
 
 ######################
 # clumping analysis

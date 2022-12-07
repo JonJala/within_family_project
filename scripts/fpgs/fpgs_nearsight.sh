8c50a8e5e50a692cfdb4b44a65b701606874337e
Tammy Tan <tammy.t133@gmail.com>
2022-12-07 15:28:05 -0500

    add fpgs scripts

---
+++ scripts/fpgs/fpgs_nearsight.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+source /var/genetics/proj/within_family/within_family_project/snipar/bin/activate
+
+within_family_path="/var/genetics/proj/within_family/within_family_project"
+snipar_path="/var/genetics/proj/within_family/within_family_project/snipar"
+
+source ${within_family_path}/scripts/fpgs/fpgipipeline_function.sh
+
+# base
+main "nearsight" "" "1" "ukb"

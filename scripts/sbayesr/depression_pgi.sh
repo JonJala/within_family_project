aa18cd6b608b6d62af770b58cf2a33edad9b0dec
Tammy Tan <tammy.t133@gmail.com>
2022-12-07 16:52:00 -0500

    add phenos to pgi

--- scripts/sbayesr/depression_pgi.sh
+++ scripts/sbayesr/depression_pgi.sh
@@ -9,8 +9,8 @@ within_family_path="/var/genetics/proj/within_family/within_family_project"
 source /var/genetics/proj/within_family/within_family_project/scripts/sbayesr/sbayesrfunc.sh
 
 # ============= Execution ============= #
-run_pgi "${within_family_path}/processed/package_output/depression/meta.sumstats.gz" "direct" "depression" "mcs"
-run_pgi "${within_family_path}/processed/package_output/depression/meta.sumstats.gz" "population" "depression" "mcs"
+run_pgi "${within_family_path}/processed/package_output/depression/meta.hm3.sumstats.gz" "direct" "depression" "mcs"
+run_pgi "${within_family_path}/processed/package_output/depression/meta.hm3.sumstats.gz" "population" "depression" "mcs"
 
 ######################
 # clumping analysis

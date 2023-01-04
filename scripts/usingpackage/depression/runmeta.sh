338cdc76a5b295fb4bff7b57745ee445d9354ef0
Tammy Tan <tammy.t133@gmail.com>
2023-01-04 11:15:48 -0500

    specify outest

--- scripts/usingpackage/depression/runmeta.sh
+++ scripts/usingpackage/depression/runmeta.sh
@@ -5,5 +5,6 @@ scriptpath="/var/genetics/proj/within_family/within_family_project/scripts/packa
 # Base
 python $scriptpath/run_metaanalysis.py \
 "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/depression/inputfiles.json" \
+--outestimates "avgparental_to_population" \
 --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/depression/meta" | tee "/var/genetics/proj/within_family/within_family_project/processed/package_output/depression/meta.log"
 

fbdf6a0e566fd5c47edac8d9357b89654f8ff88f
Tammy Tan <tammy.t133@gmail.com>
2023-01-04 11:14:52 -0500

    fix typo

--- scripts/sbayesr/hhincome_pgi.sh
+++ scripts/sbayesr/hhincome_pgi.sh
@@ -8,5 +8,5 @@ within_family_path="/var/genetics/proj/within_family/within_family_project"
 source /var/genetics/proj/within_family/within_family_project/scripts/sbayesr/sbayesrfunc.sh
 
 # ============= Execution ============= #
-run_pgi "${within_family_path}/processed/package_output/hhincome/meta.hm3.sumstats.gz" "direct" "income" "mcs"
-run_pgi "${within_family_path}/processed/package_output/hhincome/meta.hm3.sumstats.gz" "population" "income" "mcs"
+run_pgi "${within_family_path}/processed/package_output/hhincome/meta.hm3.sumstats.gz" "direct" "hhincome" "mcs"
+run_pgi "${within_family_path}/processed/package_output/hhincome/meta.hm3.sumstats.gz" "population" "hhincome" "mcs"

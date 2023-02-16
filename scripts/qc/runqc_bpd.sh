5e41e06e443261cb54b09c1633aeef34958e00a2
Tammy Tan <tammy.t133@gmail.com>
2023-02-16 17:07:08 -0500

    remove ldsc-ref from ckb qc

--- scripts/qc/runqc_bpd.sh
+++ scripts/qc/runqc_bpd.sh
@@ -99,6 +99,5 @@ python /var/genetics/proj/within_family/within_family_project/scripts/package/qc
     --outprefix "/var/genetics/proj/within_family/within_family_project/processed/qc/ckb/bpd" \
     --effects "direct_averageparental" \
     --toest "direct_population" \
-    --ldsc-ref "$reffile" \
     --af-ref "/var/genetics/proj/within_family/within_family_project/processed/qc/otherqc/eas_afs/eas_1kg.frq" \
     --cptid

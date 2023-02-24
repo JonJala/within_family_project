656cbb2c400ef6b668406fb548058598d3210792
Tammy Tan <tammy.t133@gmail.com>
2023-02-24 10:25:11 -0500

    add direct and pop heritability calc

--- scripts/qc/run_ldsc_ckb.sh
+++ scripts/qc/run_ldsc_ckb.sh
@@ -8,18 +8,39 @@ ckb_ld_chr="/var/genetics/data/ckb/private/v1/processed/ld_scores/snipar_generat
 # Activating ldsc environment
 source ${act} ${pyenv}
 
-for pheno in "bmi" "height" "bps" "bpd"
+# for pheno in "bmi" "height" "bps" "bpd"
+for pheno in "bmi" "height"
 do
 
     filein="/var/genetics/proj/within_family/within_family_project/processed/qc/ckb/${pheno}/CLEANED.out.gz" # qc'd ckb sumstats for munging
     reffile="/var/genetics/proj/within_family/within_family_project/processed/reference_samples/bbj_ref/${pheno}_ref/${pheno}_ref.sumstats.gz" # BBJ reference sumstats
     outprefix="/var/genetics/proj/within_family/within_family_project/processed/qc/ckb/${pheno}" # path where ldsc output is saved
 
-    # munge ckb sumstats
+    # munge ckb sumstats for population effects 
     python ${ldsc_path}/munge_sumstats.py \
         --sumstats ${filein} \
         --out ${outprefix}/munged_ecf \
         --signed-sumstats z_population,0 --p PVAL_population --N-col n_population
+
+    # munge ckb sumstats for direct effects 
+    python ${ldsc_path}/munge_sumstats.py \
+        --sumstats ${filein} \
+        --out ${outprefix}/munged_ecf_direct \
+        --signed-sumstats z_direct,0 --p PVAL_direct --N-col n_direct
+
+    # calculate population h2
+    python ${ldsc_path}/ldsc.py \
+        --h2 ${outprefix}/munged_ecf.sumstats.gz \
+        --ref-ld-chr ${ckb_ld_chr} \
+        --w-ld-chr ${ckb_ld_chr} \
+        --out ${outprefix}/h2_population
+
+    # calculate direct h2
+    python ${ldsc_path}/ldsc.py \
+        --h2 ${outprefix}/munged_ecf_direct.sumstats.gz \
+        --ref-ld-chr ${ckb_ld_chr} \
+        --w-ld-chr ${ckb_ld_chr} \
+        --out ${outprefix}/h2_direct
 
     # calculate rg with reference
     python ${ldsc_path}/ldsc.py \

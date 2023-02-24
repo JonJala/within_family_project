7c885f61895c8bbc86ca969e099d5af2163aca6d
Tammy Tan <tammy.t133@gmail.com>
2023-02-24 10:32:40 -0500

    remove neff

--- scripts/prscs/height_pgi.sh
+++ scripts/prscs/height_pgi.sh
@@ -6,5 +6,5 @@ within_family_path="/var/genetics/proj/within_family/within_family_project"
 source /var/genetics/proj/within_family/within_family_project/scripts/prscs/prscsfunc.sh
 
 # ============= Execution ============= #
-run_pgi "${within_family_path}/processed/package_output/height/meta.sumstats.gz" "direct" "height" "mcs" "44704"
-run_pgi "${within_family_path}/processed/package_output/height/meta.sumstats.gz" "population" "height" "mcs" "68602"
+run_pgi "${within_family_path}/processed/package_output/height/meta.sumstats.gz" "direct" "height" "mcs"
+run_pgi "${within_family_path}/processed/package_output/height/meta.sumstats.gz" "population" "height" "mcs"

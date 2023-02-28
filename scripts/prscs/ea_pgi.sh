adaf1410792edfc1e0aff8dbc466ed494ceafdfd
Tammy Tan <tammy.t133@gmail.com>
2023-02-28 17:05:32 -0500

    uncomment

--- scripts/prscs/ea_pgi.sh
+++ scripts/prscs/ea_pgi.sh
@@ -6,5 +6,5 @@ within_family_path="/var/genetics/proj/within_family/within_family_project"
 source /var/genetics/proj/within_family/within_family_project/scripts/prscs/prscsfunc.sh
 
 # ============= Execution ============= #
-# run_pgi "${within_family_path}/processed/package_output/ea/meta.sumstats.gz" "direct" "ea" "mcs"
+run_pgi "${within_family_path}/processed/package_output/ea/meta.sumstats.gz" "direct" "ea" "mcs"
 run_pgi "${within_family_path}/processed/package_output/ea/meta.sumstats.gz" "population" "ea" "mcs"

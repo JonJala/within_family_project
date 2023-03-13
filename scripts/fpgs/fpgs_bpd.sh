f2f07710d1d3b2b2f3fb41b1a00f4e372c6d6bbe
Tammy Tan <tammy.t133@gmail.com>
2023-03-13 11:41:11 -0400

    switch to wf copy of snipar effect reg

--- scripts/fpgs/fpgs_bpd.sh
+++ scripts/fpgs/fpgs_bpd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source /var/genetics/proj/within_family/snipar_venv/bin/activate
 within_family_path="/var/genetics/proj/within_family/within_family_project"
-snipar_path="/var/genetics/proj/within_family/snipar"
+snipar_path="/var/genetics/proj/within_family/snipar_effect_reg"
 
 source ${within_family_path}/scripts/fpgs/fpgipipeline_function.sh
 
--- scripts/fpgs/fpgs_nearsight.sh
+++ scripts/fpgs/fpgs_nearsight.sh
@@ -2,7 +2,7 @@
 source /var/genetics/proj/within_family/snipar_venv/bin/activate
 
 within_family_path="/var/genetics/proj/within_family/within_family_project"
-snipar_path="/var/genetics/proj/within_family/snipar"
+snipar_path="/var/genetics/proj/within_family/snipar_effect_reg"
 
 source ${within_family_path}/scripts/fpgs/fpgipipeline_function.sh
 

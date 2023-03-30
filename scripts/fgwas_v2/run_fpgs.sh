443ff2cdc27db50ba0e2d507651a3615a363457e
Tammy Tan <tammy.t133@gmail.com>
2023-03-29 20:54:23 -0400

    direct and pop estimation for unified ss

--- scripts/fgwas_v2/run_fpgs.sh
+++ scripts/fgwas_v2/run_fpgs.sh
@@ -18,10 +18,10 @@ phenofile="/var/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/i
 
 source /var/genetics/proj/within_family/within_family_project/scripts/fgwas_v2/fpgipipeline_function_fgwas.sh
 
-## unified
-# main "bmi" "" "0" "unified" "dir_pop"
-# main "height" "" "0" "unified" "dir_pop"
-# main "ea" "" "0" "unified" "dir_pop"
+# unified
+main "bmi" "" "0" "unified" "dir_pop"
+main "height" "" "0" "unified" "dir_pop"
+main "ea" "" "0" "unified" "dir_pop"
 
 # ## robust
 # main "bmi" "" "0" "robust"
@@ -30,10 +30,10 @@ source /var/genetics/proj/within_family/within_family_project/scripts/fgwas_v2/f
 
 ## sibdiff
 # main "bmi" "" "0" "sibdiff"
-main "height" "" "0" "sibdiff"
+# main "height" "" "0" "sibdiff"
 # main "ea" "" "0" "sibdiff"
 
-## young
-main "bmi" "" "0" "young"
-main "height" "" "0" "young"
-main "ea" "" "0" "young"
+# ## young
+# main "bmi" "" "0" "young"
+# main "height" "" "0" "young"
+# main "ea" "" "0" "young"

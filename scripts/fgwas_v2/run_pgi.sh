10d9102a64b2f56275ed77d415766a863f83980d
Tammy Tan <tammy.t133@gmail.com>
2023-03-30 23:40:28 -0400

    add ancestry

--- scripts/fgwas_v2/run_pgi.sh
+++ scripts/fgwas_v2/run_pgi.sh
@@ -4,29 +4,35 @@ source /var/genetics/proj/within_family/within_family_project/scripts/fgwas_v2/p
 
 # ============= Execution ============= #
 
-# ## unified
+# for ancestry in "eur" "sas"
+for ancestry in "sas"
+do
 
-# direct
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/height/merged.sumstats.gz" "direct" "height" "unified"
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/EA/merged.sumstats.gz" "direct" "ea" "unified"
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/BMI/merged.sumstats.gz" "direct" "bmi" "unified"
+    ## unified
 
-# population
-run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/height/merged.sumstats.gz" "population" "height" "unified"
-run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/EA/merged.sumstats.gz" "population" "ea" "unified"
-run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/BMI/merged.sumstats.gz" "population" "bmi" "unified"
+    # direct
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/height/merged.sumstats.gz" "direct" "height" "unified" ${ancestry}
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/EA/merged.sumstats.gz" "direct" "ea" "unified" ${ancestry}
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/BMI/merged.sumstats.gz" "direct" "bmi" "unified" ${ancestry}
 
-# ## robust
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/robust/with_grm/height/merged.sumstats.gz" "direct" "height" "robust"
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/robust/with_grm/BMI/merged.sumstats.gz" "direct" "bmi" "robust"
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/robust/with_grm/EA/merged.sumstats.gz" "direct" "ea" "robust"
+    # population
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/height/merged.sumstats.gz" "population" "height" "unified" ${ancestry}
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/EA/merged.sumstats.gz" "population" "ea" "unified" ${ancestry}
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/unified/BMI/merged.sumstats.gz" "population" "bmi" "unified" ${ancestry}
 
-# ## sibdiff
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/sibdiff/with_grm/height/merged.sumstats.gz" "direct" "height" "sibdiff"
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/sibdiff/with_grm/EA/merged.sumstats.gz" "direct" "ea" "sibdiff"
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/sibdiff/with_grm/BMI/merged.sumstats.gz" "direct" "bmi" "sibdiff"
+    ## robust
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/robust/with_grm/height/merged.sumstats.gz" "direct" "height" "robust" ${ancestry}
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/robust/with_grm/BMI/merged.sumstats.gz" "direct" "bmi" "robust" ${ancestry}
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/robust/with_grm/EA/merged.sumstats.gz" "direct" "ea" "robust" ${ancestry}
 
-## young
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/young/height/merged.sumstats.gz" "direct" "height" "young"
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/young/EA/merged.sumstats.gz" "direct" "ea" "young"
-# run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/young/BMI/merged.sumstats.gz" "direct" "bmi" "young"
+    ## sibdiff
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/sibdiff/with_grm/height/merged.sumstats.gz" "direct" "height" "sibdiff" ${ancestry}
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/sibdiff/with_grm/EA/merged.sumstats.gz" "direct" "ea" "sibdiff" ${ancestry}
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/sibdiff/with_grm/BMI/merged.sumstats.gz" "direct" "bmi" "sibdiff" ${ancestry}
+
+    # young
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/young/height/merged.sumstats.gz" "direct" "height" "young" ${ancestry}
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/young/EA/merged.sumstats.gz" "direct" "ea" "young" ${ancestry}
+    run_pgi "/disk/genetics/ukb/jguan/ukb_analysis/output/gwas/v2/paper_results/young/BMI/merged.sumstats.gz" "direct" "bmi" "young" ${ancestry}
+
+done

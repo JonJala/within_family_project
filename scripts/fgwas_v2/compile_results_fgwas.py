a9da5fc6c59cee42f7afd19f1797acf7751dabe0
Tammy Tan <tammy.t133@gmail.com>
2023-04-06 11:39:39 -0400

    add sas ancestry

--- scripts/fgwas_v2/compile_results_fgwas.py
+++ scripts/fgwas_v2/compile_results_fgwas.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import numpy as np
-import subprocess
-import json
 
 basepath = '/var/genetics/proj/within_family/within_family_project/'
 fpgspath = basepath + 'processed/fpgs/'
@@ -15,75 +15,113 @@ datfpgs = pd.DataFrame(columns=['phenotype', 'effect', 'direct', 'direct_se',
                 'parental_pgi_corr', 'parental_pgi_corr_se'])
 fpgsfiles = [fpgspath + ph for ph in phenotypes]
 
+# ancestry = "eur"
+ancestry = "sas"
+
 # make table of results
-# for method in ['unified', 'robust', 'sibdiff', 'young']:
-for method in ['unified']:
-    for phenotype in phenotypes:
-        # for effect in ['direct']:
-        for effect in ['population']:
-        
-            print(f'Compiling results for {method} {phenotype} {effect}...')
-            packageoutput = basepath + 'processed/package_output/'
+if ancestry == "eur":
+    for method in ['unified', 'robust', 'sibdiff', 'young']:
+    # for method in ['unified']:
+        for phenotype in phenotypes:
+            for effect in ['direct']:
+            # for effect in ['population']:
+            
+                print(f'Compiling results for {method} {phenotype} {effect}...')
 
-            # fpgs results
-            proband = pd.read_csv(
-                basepath + 'processed/fpgs/' + phenotype + f'/{method}/{effect}_proband.pgs_effects.txt',
-                delim_whitespace=True,
-                names = ['coeff', 'se', 'r2']
-            )
+                # fpgs results
+                proband = pd.read_csv(
+                    basepath + 'processed/fpgs/' + phenotype + f'/{method}/{ancestry}/{effect}_proband.pgs_effects.txt',
+                    delim_whitespace=True,
+                    names = ['coeff', 'se', 'r2']
+                )
 
-            full = pd.read_csv(
-                basepath + 'processed/fpgs/' + phenotype + f'/{method}/{effect}_full.pgs_effects.txt',
-                delim_whitespace=True,
-                names = ['coeff', 'se', 'r2']
-            )
+                full = pd.read_csv(
+                    basepath + 'processed/fpgs/' + phenotype + f'/{method}/{ancestry}/{effect}_full.pgs_effects.txt',
+                    delim_whitespace=True,
+                    names = ['coeff', 'se', 'r2']
+                )
 
-            covariates_only = pd.read_csv(
-                basepath + 'processed/fpgs/' + phenotype + '/' + method + '/covariates.pgs_effects.txt',
-                delim_whitespace=True,
-                names = ['coeff', 'se', 'r2']
-            )
+                covariates_only = pd.read_csv(
+                    basepath + 'processed/fpgs/' + phenotype + '/' + method + '/' + ancestry + '/covariates.pgs_effects.txt',
+                    delim_whitespace=True,
+                    names = ['coeff', 'se', 'r2']
+                )
 
-            direst = full.loc['proband', 'coeff']
-            dirse = full.loc['proband', 'se']
-            patest = full.loc['paternal', 'coeff']
-            patse = full.loc['paternal', 'se']
-            matest = full.loc['maternal', 'coeff']
-            matse = full.loc['maternal', 'se']
-            popest = proband.loc['proband', 'coeff']
-            popse = proband.loc['proband', 'se']
+                direst = full.loc['proband', 'coeff']
+                dirse = full.loc['proband', 'se']
+                patest = full.loc['paternal', 'coeff']
+                patse = full.loc['paternal', 'se']
+                matest = full.loc['maternal', 'coeff']
+                matse = full.loc['maternal', 'se']
+                popest = proband.loc['proband', 'coeff']
+                popse = proband.loc['proband', 'se']
 
-            incrr2_proband = proband.loc['proband', 'r2'] - covariates_only.loc['age', 'r2']
-            incrr2_full = full.loc['proband', 'r2'] - covariates_only.loc['age', 'r2']
+                incrr2_proband = proband.loc['proband', 'r2'] - covariates_only.loc['age', 'r2']
+                incrr2_full = full.loc['proband', 'r2'] - covariates_only.loc['age', 'r2']
 
-            # parental PGI correlation
-            parcorr = np.loadtxt(
-                basepath + 'processed/fgwas_v2/' + method + '/' + phenotype + '/' + effect + '/.parentalcorr'
-            )
+                # parental PGI correlation
+                parcorr = np.loadtxt(
+                    basepath + 'processed/fgwas_v2/' + method + '/' + phenotype + '/' + effect + '/.parentalcorr'
+                )
 
-            parcorr_est = parcorr[0]
-            parcorr_se = parcorr[1]
+                parcorr_est = parcorr[0]
+                parcorr_se = parcorr[1]
 
-            datfpgs_tmp = pd.DataFrame({
-                    'method' : [method],
-                    'phenotype' : [phenotype],
-                    'effect' : [effect],
-                    'direct' : [direst],
-                    'direct_se' : [dirse],
-                    'pop' : [popest],
-                    'pop_se' : [popse],
-                    'paternal' : [patest],
-                    'paternal_se' : [patse],
-                    'maternal' : [matest],
-                    'maternal_se' : [matse],
-                    'incr_r2_proband' : [incrr2_proband],
-                    'incr_r2_full' : [incrr2_full],
-                    'parental_pgi_corr' : [parcorr_est],
-                    'parental_pgi_corr_se' : [parcorr_se]
-            })
+                datfpgs_tmp = pd.DataFrame({
+                        'method' : [method],
+                        'phenotype' : [phenotype],
+                        'effect' : [effect],
+                        'direct' : [direst],
+                        'direct_se' : [dirse],
+                        'pop' : [popest],
+                        'pop_se' : [popse],
+                        'paternal' : [patest],
+                        'paternal_se' : [patse],
+                        'maternal' : [matest],
+                        'maternal_se' : [matse],
+                        'incr_r2_proband' : [incrr2_proband],
+                        'incr_r2_full' : [incrr2_full],
+                        'parental_pgi_corr' : [parcorr_est],
+                        'parental_pgi_corr_se' : [parcorr_se]
+                })
 
-            datfpgs = datfpgs.append(datfpgs_tmp, ignore_index=True)
+                datfpgs = datfpgs.append(datfpgs_tmp, ignore_index=True)
+elif ancestry == "sas":
+    for method in ['unified', 'robust', 'sibdiff', 'young']:
+    # for method in ['unified']:
+        for phenotype in phenotypes:
+            for effect in ['direct']:
+            # for effect in ['population']:
+            
+                print(f'Compiling results for {method} {phenotype} {effect}...')
 
+                # fpgs results
+                proband = pd.read_csv(
+                    basepath + 'processed/fpgs/' + phenotype + f'/{method}/{ancestry}/{effect}_proband.pgs_effects.txt',
+                    delim_whitespace=True,
+                    names = ['coeff', 'se', 'r2']
+                )
+
+                covariates_only = pd.read_csv(
+                    basepath + 'processed/fpgs/' + phenotype + '/' + method + '/' + ancestry + '/covariates.pgs_effects.txt',
+                    delim_whitespace=True,
+                    names = ['coeff', 'se', 'r2']
+                )
+
+                popest = proband.loc['proband', 'coeff']
+                popse = proband.loc['proband', 'se']
+                incrr2_proband = proband.loc['proband', 'r2'] - covariates_only.loc['age', 'r2']
+
+                datfpgs_tmp = pd.DataFrame({
+                        'method' : [method],
+                        'phenotype' : [phenotype],
+                        'effect' : [effect],
+                        'pop' : [popest],
+                        'pop_se' : [popse],
+                        'incr_r2_proband' : [incrr2_proband],
+                })
+
+                datfpgs = datfpgs.append(datfpgs_tmp, ignore_index=True)
 
 floatcols = [c for c in datfpgs.columns if (c not in ['method', 'phenotype', 'effect'])]
 datfpgs[floatcols] = datfpgs[floatcols].apply(pd.to_numeric)
@@ -92,5 +130,5 @@ datfpgs = datfpgs.reorder_levels(['effect', None], axis=1)
 datfpgs = datfpgs.sort_index(axis=1, level=0, sort_remaining=False)
 datfpgs.columns = ['_'.join(column) for column in datfpgs.columns.to_flat_index()]
 datfpgs.to_excel(
-    '/var/genetics/proj/within_family/within_family_project/processed/fgwas_v2/fgwas_fpgs_results.xlsx'
+    f'/var/genetics/proj/within_family/within_family_project/processed/fgwas_v2/fgwas_fpgs_results_{ancestry}.xlsx'
 )

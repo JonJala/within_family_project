882a5146658a0c832911a13acd07c96effd5f330
Tammy Tan <tammy.t133@gmail.com>
2023-04-11 11:46:02 -0400

    script to prepare phenos for fgwas

--- scripts/fgwas_v2/prepare_phenotypes_fgwas.R
+++ scripts/fgwas_v2/prepare_phenotypes_fgwas.R
@@ -15,7 +15,7 @@ library(foreign)
 # 2. select the pheno and two ID columns
 # 3. rename the two ID columns to IID and FID, where FID = IID = Benjamin ID x 2
 # 4. get rid of the Benjamin ID columns
-# 5. perform whatever processing you need on the pheno, e.g. standardization, renaming, drop NAs, etc.
+# 5. perform whatever processing you need on the pheno.
 # 6. merge all the phenos by FID and IID
 # 7. save
 
@@ -83,21 +83,32 @@ setnames(ht_bmi, old=c("height7", "bmi7"), new=c("height", "bmi"))
 phenotypes = reduce(list(ht_bmi, ea), merge, by = c("FID", "IID"), all=TRUE)
 phenotypes = merge(phenotypes, covariates, by = c("FID", "IID"), all=TRUE)
 
-## filter to just get SAS sample
-sample = fread("/disk/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/filter_extract/sas_samples_mz_removed.txt", col.names = c("FID", "IID"))
-phenotypes %<>% filter(IID %in% sample$IID)
+filter_and_std <- function(data, sample_path) {
 
-## standardize
+    ## filter to just get relevant sample
+    sample = fread(sample_path, col.names = c("FID", "IID"))
+    data %<>% filter(IID %in% sample$IID)
 
-# standardize bmi and height by sex
-phenotypes[, bmi := standardize(bmi), by=sex]
-phenotypes[, height := standardize(height), by=sex]
+    ## standardize
 
-# standardize ea
-phenotypes[, ea := standardize(ea)]
+    # standardize bmi and height by sex
+    data[, bmi := standardize(bmi), by=sex]
+    data[, height := standardize(height), by=sex]
 
-## remove duplicated rows
-phenotypes = distinct(phenotypes, .keep_all = TRUE)
+    # standardize ea
+    data[, ea := standardize(ea)]
 
-## save
-fwrite(phenotypes, file="/var/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/phen/phenotypes_sas.txt", sep=" ", na="NA", quote = FALSE)
+    ## remove duplicated rows
+    data = distinct(data, .keep_all = TRUE)
+
+    return(data)
+
+}
+
+## get eur sample
+sas_pheno <- filter_and_std(phenotypes, "/disk/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/filter_extract/sas_samples_mz_removed.txt")
+fwrite(sas_pheno, file="/var/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/phen/phenotypes_sas.txt", sep=" ", na="NA", quote = FALSE)
+
+## get eur sample
+eur_pheno <- filter_and_std(phenotypes, "/disk/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/filter_extract/eur_samples_mz_removed.txt")
+fwrite(eur_pheno, file="/var/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/phen/phenotypes_eur.txt", sep=" ", na="NA", quote = FALSE)

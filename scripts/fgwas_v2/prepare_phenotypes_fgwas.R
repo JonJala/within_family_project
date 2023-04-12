f7a539bdbcce66e8be52279bea49f5e6ba19b962
Tammy Tan <tammy.t133@gmail.com>
2023-04-12 12:38:53 -0400

    fix phenotype processing

--- scripts/fgwas_v2/prepare_phenotypes_fgwas.R
+++ scripts/fgwas_v2/prepare_phenotypes_fgwas.R
@@ -19,37 +19,11 @@ library(foreign)
 # 6. merge all the phenos by FID and IID
 # 7. save
 
-# define functions to help with processing
+## function to set mean to 0 and var to 1
 standardize <- function(x) {
     out = (x - mean(x, na.rm=TRUE))/sd(x, na.rm=TRUE)
     return(out)
 }
-
-read_and_rename <- function(pheno_code, pheno_name, file_path = "/var/genetics/data/mcs/private/latest/raw/phen/MDAC-2020-0031-05A-BENJAMIN_addtional_vars/csv/GENDAC_BENJAMIN_mcs_cm_structure_2021_10_08.csv") {
-    # read in the file
-    pheno = fread(file_path)
-    
-    # select columns
-    pheno = pheno[, c(grep(paste0("(", pheno_code, "|Benjamin*)"), names(pheno))), with=FALSE]
-
-    # rename the ID columns
-    pheno[,IID := paste(Benjamin_ID, Benjamin_ID, sep="_")]
-    pheno[,FID := IID]
-
-    # get rid of the Benjamin ID columns
-    pheno[, grep("Benjamin_", names(pheno)) := NULL]
-
-    # rename pheno
-    setnames(pheno, pheno_code, pheno_name)
-
-    # drop NAs
-    pheno = na.omit(pheno)
-
-    return(pheno)
-}
-
-# read in covariates
-covariates = fread("/var/genetics/data/mcs/private/v1/raw/genotyped/NCDS_SFTP_1TB_1/imputed/phen/covar_sas.txt")
 
 #---------------------------------------------------------------------------------------------------------------------
 # ea
@@ -81,23 +55,21 @@ setnames(ht_bmi, old=c("height7", "bmi7"), new=c("height", "bmi"))
 
 ## merge phenos
 phenotypes = reduce(list(ht_bmi, ea), merge, by = c("FID", "IID"), all=TRUE)
-phenotypes = merge(phenotypes, covariates, by = c("FID", "IID"), all=TRUE)
 
-filter_and_std <- function(data, sample_path) {
+filter_and_std <- function(data, sample_path, covariates_path) {
 
     ## filter to just get relevant sample
     sample = fread(sample_path, col.names = c("FID", "IID"))
     data %<>% filter(IID %in% sample$IID)
 
-    ## standardize
+    ## merge with covariates
+    covariates = fread(covariates_path, sep = "\t")
+    data = left_join(data, covariates, by = c("FID", "IID"))
 
-    # standardize bmi and height by sex
+    ## standardize bmi and height by sex to have mean 0 and variance 1
     data[, bmi := standardize(bmi), by=sex]
     data[, height := standardize(height), by=sex]
-
-    # standardize ea
-    data[, ea := standardize(ea)]
-
+    
     ## remove duplicated rows
     data = distinct(data, .keep_all = TRUE)
 
@@ -105,10 +77,10 @@ filter_and_std <- function(data, sample_path) {
 
 }
 
-## get eur sample
-sas_pheno <- filter_and_std(phenotypes, "/disk/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/filter_extract/sas_samples_mz_removed.txt")
+## get sas sample
+sas_pheno <- filter_and_std(phenotypes, sample_path = "/disk/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/filter_extract/sas_samples_mz_removed.txt", covariates_path = "/var/genetics/data/mcs/private/v1/raw/genotyped/NCDS_SFTP_1TB_1/imputed/phen/covar_sas.txt")
 fwrite(sas_pheno, file="/var/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/phen/phenotypes_sas.txt", sep=" ", na="NA", quote = FALSE)
 
 ## get eur sample
-eur_pheno <- filter_and_std(phenotypes, "/disk/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/filter_extract/eur_samples_mz_removed.txt")
+eur_pheno <- filter_and_std(phenotypes, sample_path = "/disk/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/filter_extract/eur_samples_mz_removed.txt", covariates_path = "/var/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/phen/covar.txt")
 fwrite(eur_pheno, file="/var/genetics/data/mcs/private/latest/raw/genotyped/NCDS_SFTP_1TB_1/imputed/phen/phenotypes_eur.txt", sep=" ", na="NA", quote = FALSE)

13999dbf08074d40de34779961629800359c6094
Tammy Tan <tammy.t133@gmail.com>
2023-04-24 14:33:59 -0400

    fix ipsych ss paths

--- scripts/qc/runqc_hayfever.sh
+++ scripts/qc/runqc_hayfever.sh
@@ -34,7 +34,7 @@ reffile="/var/genetics/proj/within_family/within_family_project/processed/refere
 ############
 
 python /var/genetics/proj/within_family/within_family_project/scripts/package/qc/run_easyqc.py \
-    "/var/genetics/data/ipsych/public/v1/processed/sumstats/sumstats_hayfever_export0.txt" \
+    "/var/genetics/data/ipsych/public/latest/processed/sumstats/sumstats_hayfever_export0.txt" \
     --outprefix "/var/genetics/proj/within_family/within_family_project/processed/qc/ipsych/hayfever" \
     --toest "direct_population" \
     --cptid \

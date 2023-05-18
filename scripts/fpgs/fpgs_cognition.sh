db16ff9f17b95111e977ec89b583f46c514e7914
Tammy Tan <tammy.t133@gmail.com>
2023-05-18 14:36:36 -0400

    remove path

--- scripts/fpgs/fpgs_cognition.sh
+++ scripts/fpgs/fpgs_cognition.sh
@@ -2,7 +2,6 @@
 source /var/genetics/proj/within_family/snipar_venv/bin/activate
 
 within_family_path="/var/genetics/proj/within_family/within_family_project"
-snipar_path="/var/genetics/proj/within_family/snipar_simulate"
 
 source ${within_family_path}/scripts/fpgs/fpgipipeline_function.sh
 

1eb8d9feec369e70c4c1a7fd7f8cc391a5b485b8
Tammy Tan <tammy.t133@gmail.com>
2023-06-15 09:42:35 -0400

    add finngen to meta

--- scripts/usingpackage/eczema/runmeta.sh
+++ scripts/usingpackage/eczema/runmeta.sh
@@ -4,6 +4,5 @@ scriptpath="/var/genetics/proj/within_family/within_family_project/scripts/packa
 
 python $scriptpath/run_metaanalysis.py \
 "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/eczema/inputfiles.json" \
---outestimates "avgparental_to_population" \
 --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/eczema/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/eczema/meta.log
 

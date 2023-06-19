c10fb0c3baf9d208c04341e075d22ab1e1c40790
Tammy Tan <tammy.t133@gmail.com>
2023-06-19 17:42:52 -0400

    correlation re meta

---
+++ scripts/qc/finngen_ldsc_rg.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+pheno="bmi"
+sumstats="/var/genetics/proj/within_family/within_family_project/processed/qc/finngen/${pheno}/CLEANED.out.gz"
+ldsc_ref="/var/genetics/proj/within_family/within_family_project/processed/reference_samples/bmi_ref/Meta-analysis_Locke_et_al+UKBiobank_2018_UPDATED.sumstats.gz"
+ldscores="/disk/genetics/data/finngen/private/v1/processed/ld_scores/"    
+merge_alleles="/disk/genetics2/pub/data/PH3_Reference/w_hm3.snplist"
+ldsc_path="/var/genetics/tools/ldsc/ldsc"
+outpath="/var/genetics/proj/within_family/within_family_project/processed/qc/finngen/${pheno}"
+
+act="/disk/genetics/pub/python_env/anaconda2/bin/activate"
+pyenv="/disk/genetics/pub/python_env/anaconda2/envs/ldsc"
+
+# Activating ldsc environment
+source ${act} ${pyenv}
+
+# munging data
+python ${ldsc_path}/munge_sumstats.py \
+    --sumstats ${sumstats} \
+    --merge-alleles ${merge_alleles} \
+    --out ${outpath}/munged_ecf \
+    --signed-sumstats z_population,0 --p PVAL_population --N-col n_population
+
+# calculate rg
+python ${ldsc_path}/ldsc.py \
+    --rg ${outpath}/munged_ecf.sumstats.gz,${ldsc_ref} \
+    --ref-ld-chr ${ldscores} \
+    --w-ld-chr ${ldscores} \
+    --out ${outpath}/refldsc
+
+
--- scripts/usingpackage/ea/correlation_analysis.sh
+++ scripts/usingpackage/ea/correlation_analysis.sh
@@ -4,6 +4,7 @@ within_family_path="/var/genetics/proj/within_family/within_family_project"
 snipar_path="/var/genetics/proj/within_family/snipar_simulate/snipar"
 basepath="/var/genetics/proj/within_family/within_family_project/scripts/package"
 pheno="ea"
+sumstats="/var/genetics/data/ukb/private/v3/processed/proj/within_family/sumstats/EA/EA"
 # cohorts="ukb str moba minn_twins lifelines hunt gs geisinger ft estonian_biobank dutch_twin botnia qimr"
 cohorts="ukb"
 
@@ -12,7 +13,7 @@ source /var/genetics/proj/within_family/snipar_venv/bin/activate
 ## calculate direct-pop correlation for each cohort
 for cohort in ${cohorts}
 do
-    ${snipar_path}/scripts/correlate.py  ${within_family_path}/processed/qc/${cohort}/${pheno}/CLEANED.out \
+    ${snipar_path}/scripts/correlate.py ${sumstats} \
         ${within_family_path}/processed/qc/${cohort}/${pheno}/marginal_correlations \
         --ldscores /disk/genetics/ukb/alextisyoung/hapmap3/ldscores/@
 done

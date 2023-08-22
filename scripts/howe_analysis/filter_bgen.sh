285b84e71474a6b80eed16fdf38372715cc2991a
Tammy Tan <tammy.t133@gmail.com>
2023-08-21 21:20:58 -0400

    change file paths

--- scripts/howe_analysis/filter_bgen.sh
+++ scripts/howe_analysis/filter_bgen.sh
@@ -7,15 +7,15 @@
 ## low info snps
 plink2 --bgen /disk/genetics2/ukb/orig/UKBv3/imputed_data/ukb_imp_chr1_v3.bgen ref-first \
     --sample /disk/genetics2/ukb/orig/UKBv3/sample/ukb11425_imp_chr1_22_v3_s487395.sample \
-    --keep /disk/genetics4/ukb/tammytan/wf/howe_analysis/all_sibs.txt \
-    --extract /disk/genetics4/ukb/tammytan/wf/howe_analysis/low_info_snps.txt \
+    --keep /disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_analysis/all_sibs.txt \
+    --extract /disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_analysis/low_info_snps.txt \
     --make-bed \
-    --out /disk/genetics4/ukb/tammytan/wf/howe_analysis/all_sibs_low_info
+    --out /disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_analysis/all_sibs_low_info
 
-# ## high info SNPs
-# plink2 --bgen /disk/genetics2/ukb/orig/UKBv3/imputed_data/ukb_imp_chr1_v3.bgen ref-first \
-#     --sample /disk/genetics2/ukb/orig/UKBv3/sample/ukb11425_imp_chr1_22_v3_s487395.sample \
-#     --keep /disk/genetics4/ukb/tammytan/wf/howe_analysis/all_sibs.txt \
-#     --extract /disk/genetics4/ukb/tammytan/wf/howe_analysis/high_info_snps.txt \
-#     --make-bed \
-#     --out /disk/genetics4/ukb/tammytan/wf/howe_analysis/all_sibs_high_info
+## high info SNPs
+plink2 --bgen /disk/genetics2/ukb/orig/UKBv3/imputed_data/ukb_imp_chr1_v3.bgen ref-first \
+    --sample /disk/genetics2/ukb/orig/UKBv3/sample/ukb11425_imp_chr1_22_v3_s487395.sample \
+    --keep /disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_analysis/all_sibs.txt \
+    --extract /disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_analysis/high_info_snps.txt \
+    --make-bed \
+    --out /disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_analysis/all_sibs_high_info

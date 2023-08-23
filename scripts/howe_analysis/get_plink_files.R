ca87daa1516ebea1c3feef052097c2d2228cecac
Tammy Tan <tammy.t133@gmail.com>
2023-08-23 14:21:11 -0400

    analyze snps with info from 0.3 to 0.99

--- scripts/howe_analysis/get_plink_files.R
+++ scripts/howe_analysis/get_plink_files.R
@@ -18,19 +18,24 @@ set.seed(42)
 ## list of snps to keep
 
 # read in chr 1 data
-dat <- fread("/disk/genetics2/ukb/orig/UKBv3/imputed_data/ukb_mfi_chr1_v3.txt")
+dat <- fread("/disk/genetics2/ukb/orig/UKBv3/imputed_data/ukb_mfi_chr1_v3.txt") # 7.4 million SNPs
+dat %<>% filter(V6 > 0.01) # filter MAF > 0.01 as per howe et al.; 772k SNPs left
 
-# filter to low info snps
-low_info <- dat %>%
-                filter(0.3 < V8 & V8 < 0.31) # filter info score between 0.3 and 0.31
-low_info_snps <- sample(low_info$V2, 100, replace = FALSE) # randomly select 100 snps
-fwrite(data.frame(low_info_snps), "/disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_analysis/low_info_snps.txt", col.names = FALSE, row.names = FALSE, quote = FALSE)
-
-# high info snps
-high_info <- dat %>%
-                filter(V8 > 0.99)
-high_info_snps <- sample(high_info$V2, 100, replace = FALSE)
-fwrite(data.frame(high_info_snps), "/disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_analysis/high_info_snps.txt", col.names = FALSE, row.names = FALSE, quote = FALSE)
+# filter to snps with info score 0.3-0.99 at 0.01 intervals
+for (i in seq(0.3, 0.99, 0.01)) {
+    j <- i + 0.01
+    info <- dat %>%
+            filter(i < V8 & V8 < j) # filter info score
+    if (nrow(info) >= 1000) {
+        print(paste0("Randomly selecting 1k SNPs with INFO between ", i, " and ", j, "."))
+        snps <- sample(info$V2, 1000, replace = FALSE) # randomly select 1000 snps
+    } else {
+        print(paste0("Less than 1k SNPs available, selecting all ", nrow(info), " SNPs with INFO between ", i, " and ", j, "."))
+        snps <- info$V2 # select all SNPs available
+    }
+    k <- str_replace(as.character(i), "\\.", "_")
+    fwrite(data.frame(snps), paste0("/disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_info_analysis/keep/snps_", k, ".txt"), col.names = FALSE, row.names = FALSE, quote = FALSE)
+}
 
 ## ---------------------------------------------------------------------
 ## create sample file
@@ -42,4 +47,4 @@ sibs %<>% filter(InfType == "FS") # full sibs only
 sib1 <- data.frame(sibs$FID1, sibs$ID1, fix.empty.names =  FALSE)
 sib2 <- data.frame(sibs$FID2, sibs$ID2, fix.empty.names =  FALSE)
 all_sibs <- rbind(sib1, sib2)
-fwrite(all_sibs, "/disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_analysis/all_sibs.txt", col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t")
+fwrite(all_sibs, "/disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_info_analysis/all_sibs.txt", col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t")

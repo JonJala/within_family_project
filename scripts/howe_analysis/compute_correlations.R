a2af44785a7ae31b3f81473e5b7a45468b246990
Tammy Tan <tammy.t133@gmail.com>
2023-09-07 14:59:45 -0400

    add n snps, reformat using ggplot

--- scripts/howe_analysis/compute_correlations.R
+++ scripts/howe_analysis/compute_correlations.R
@@ -4,7 +4,7 @@
 ## description: compute gcs between sibs for howe et al info score analysis
 ## ---------------------------------------------------------------------
 
-list.of.packages <- c("data.table", "dplyr", "magrittr", "tidyverse")
+list.of.packages <- c("data.table", "dplyr", "magrittr", "tidyverse", "ggplot2")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos = "http://cran.rstudio.com/")
 lapply(list.of.packages, library, character.only = TRUE)
@@ -49,24 +49,36 @@ sibs %<>% filter(InfType == "FS") # full sibs only
 ## compute correlations
 raw_path <- "/disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_info_analysis/plink_out"
 raw_files <- list.files(path = raw_path, pattern = ".raw")
-corr_df <- data.frame(info = NULL, mean = NULL, sd = NULL, n_na = NULL, stringsAsFactors = FALSE)
+corr_df <- data.frame(info = NULL, mean = NULL, sd = NULL, n_na = NULL, n_snps = NULL, stringsAsFactors = FALSE)
 for (file in raw_files) {
     
     print(paste0("Computing correlations for ", file, "."))
     raw <- paste0(raw_path, "/", file)
     corrs <- compute_corrs(raw, sibs)
     f <- str_replace(file, ".raw", "")
+    info <- f %>%
+                str_replace("snps_", "") %>%
+                str_replace("_", ".")
+
+    # get n snps
+    keep <- fread(paste0("/disk/genetics3/data_dirs/ukb/private/v3/processed/proj/within_family/howe_info_analysis/keep/", f, ".txt"), header = FALSE)
+    n_snps <- nrow(keep)
 
     # plot histogram
-    png(paste0("/disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_info_analysis/output/", f, "_hist.png"))
-    hist(corrs)
-    dev.off()
+    ggplot(data.frame(corrs), aes(x = corrs)) +
+        geom_histogram(binwidth = 0.01, fill = "#56B4E9", colour = "#56B4E9", alpha = 0.5) +
+        labs(title = paste0("Genetic Correlations for SNPS with INFO = ", info, " (n = ", n_snps, ")"),
+             x = "Genetic Correlation",
+             y = "Count") +
+        theme_classic() +
+        theme(legend.position="none")
+    ggsave(paste0("/disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_info_analysis/output/", f, "_hist.png"))
 
     # add summary stats to table
     mean <- mean(corrs, na.rm = T)
     sd <- sd(corrs, na.rm = T)
     n_na <- sum(is.na(corrs))
-    corr_df <- rbind(corr_df, data.frame(info = f, mean = mean, sd = sd, n_na = n_na, stringsAsFactors = FALSE))
+    corr_df <- rbind(corr_df, data.frame(info = f, mean = mean, sd = sd, n_na = n_na, n_snps = n_snps, stringsAsFactors = FALSE))
 
 }
 fwrite(corr_df, "/disk/genetics/data/ukb/private/latest/processed/proj/within_family/howe_info_analysis/output/corrs.csv", col.names = TRUE, row.names = FALSE, quote = FALSE)

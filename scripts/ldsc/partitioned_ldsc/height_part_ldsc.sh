20c8cd2d6cd180f5e6a1d4615a89aefd7264c158
Tammy Tan <tammy.t133@gmail.com>
2023-09-22 13:17:08 -0400

    partitioned ldsc for height

---
+++ scripts/ldsc/partitioned_ldsc/height_part_ldsc.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/bash
+
+## partitioned LDSC analysis for h2. based off /disk/genetics4/projects/EA4/code/partitionedLDSC/runldsc.sh
+
+pheno="height"
+effect="direct"
+ldscpath="/disk/genetics2/pub/repo/ssgac/ldsc_mod"
+merge_alleles="/disk/genetics2/pub/data/PH3_Reference/w_hm3.snplist"
+ea4_dir="/disk/genetics4/projects/EA4"
+within_family_directory="/var/genetics/proj/within_family/within_family_project"
+
+# == Using Phase 3 data == #
+echo "Running Partitioned LDSC"
+python ${ldscpath}/ldsc.py \
+  --h2 ${within_family_directory}/processed/package_output/${pheno}/${effect}munged.sumstats.gz \
+  --ref-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_baselineLD_ldscores/baselineLD. \
+  --frqfile-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_frq/1000G.EUR.QC. \
+  --w-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC. \
+  --overlap-annot \
+  --print-coefficients \
+  --print-delete-vals \
+  --out ${within_family_directory}/partitioned_ldsc/${pheno}/${effect}.baselineLD
+
+
+echo "Using Cahoy et al annotations - baseline controls"
+python ${ldscpath}/ldsc.py \
+  --h2 ${within_family_directory}/processed/package_output/${pheno}/${effect}munged.sumstats.gz \
+  --ref-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/Cahoy_1000Gv3_ldscores/Cahoy.1.,${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_baselineLD_ldscores/baselineLD. \
+  --frqfile-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_frq/1000G.EUR.QC. \
+  --w-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC. \
+  --overlap-annot \
+  --print-coefficients \
+  --print-delete-vals \
+  --out ${within_family_directory}/partitioned_ldsc/${pheno}/${effect}.cahoy.1
+
+python ${ldscpath}/ldsc.py \
+  --h2 ${within_family_directory}/processed/package_output/${pheno}/${effect}munged.sumstats.gz \
+  --ref-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/Cahoy_1000Gv3_ldscores/Cahoy.2.,${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_baselineLD_ldscores/baselineLD. \
+  --frqfile-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_frq/1000G.EUR.QC. \
+  --w-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC. \
+  --overlap-annot \
+  --print-coefficients \
+  --print-delete-vals \
+  --out ${within_family_directory}/partitioned_ldsc/${pheno}/${effect}.cahoy.2
+
+python ${ldscpath}/ldsc.py \
+  --h2 ${within_family_directory}/processed/package_output/${pheno}/${effect}munged.sumstats.gz \
+  --ref-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/Cahoy_1000Gv3_ldscores/Cahoy.3.,${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_baselineLD_ldscores/baselineLD. \
+  --frqfile-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_frq/1000G.EUR.QC. \
+  --w-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC. \
+  --overlap-annot \
+  --print-coefficients \
+  --print-delete-vals \
+  --out ${within_family_directory}/partitioned_ldsc/${pheno}/${effect}.cahoy.3
+
+echo "Using Cahoy et al annotations - baseline controls + cahoy controls"
+
+python ${ldscpath}/ldsc.py \
+  --h2 ${within_family_directory}/processed/package_output/${pheno}/${effect}munged.sumstats.gz \
+  --ref-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/Cahoy_1000Gv3_ldscores/Cahoy.1.,${ea4_dir}/derived_data/partitionedLDSC/rawdata/Cahoy_1000Gv3_ldscores/Cahoy.control.,${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_baselineLD_ldscores/baselineLD. \
+  --frqfile-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_frq/1000G.EUR.QC. \
+  --w-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC. \
+  --overlap-annot \
+  --print-coefficients \
+  --print-delete-vals \
+  --out ${within_family_directory}/partitioned_ldsc/${pheno}/${effect}.cahoy.1.control
+
+
+python ${ldscpath}/ldsc.py \
+  --h2 ${within_family_directory}/processed/package_output/${pheno}/${effect}munged.sumstats.gz \
+  --ref-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/Cahoy_1000Gv3_ldscores/Cahoy.2.,${ea4_dir}/derived_data/partitionedLDSC/rawdata/Cahoy_1000Gv3_ldscores/Cahoy.control.,${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_baselineLD_ldscores/baselineLD. \
+  --frqfile-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_frq/1000G.EUR.QC. \
+  --w-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC. \
+  --overlap-annot \
+  --print-coefficients \
+  --print-delete-vals \
+  --out ${within_family_directory}/partitioned_ldsc/${pheno}/${effect}.cahoy.2.control
+
+python ${ldscpath}/ldsc.py \
+  --h2 ${within_family_directory}/processed/package_output/${pheno}/${effect}munged.sumstats.gz \
+  --ref-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/Cahoy_1000Gv3_ldscores/Cahoy.3.,${ea4_dir}/derived_data/partitionedLDSC/rawdata/Cahoy_1000Gv3_ldscores/Cahoy.control.,${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_baselineLD_ldscores/baselineLD. \
+  --frqfile-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_frq/1000G.EUR.QC. \
+  --w-ld-chr ${ea4_dir}/derived_data/partitionedLDSC/rawdata/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC. \
+  --overlap-annot \
+  --print-coefficients \
+  --print-delete-vals \
+  --out ${within_family_directory}/partitioned_ldsc/${pheno}/${effect}.cahoy.3.control
+
+
+echo "=================="
+echo "Done! :)"
+echo "=================="

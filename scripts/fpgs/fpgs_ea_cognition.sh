2809068c54a873b8987eb84af93bcf3f64993fa8
Tammy Tan <tammy.t133@gmail.com>
2023-12-10 04:30:01 -0500

    add function to bootstrap coeff ratio for ea and cog

--- scripts/fpgs/fpgs_ea_cognition.sh
+++ scripts/fpgs/fpgs_ea_cognition.sh
@@ -6,6 +6,7 @@ sniparenv="/var/genetics/proj/within_family/snipar_venv/bin/activate"
 
 source ${sniparenv}
 
+## function to run fpgs regression
 function run_fpgs_regression(){
     
     PHENONAME=$1
@@ -54,12 +55,43 @@ function run_fpgs_regression(){
 
 }
 
+
+## function to get bootstrap estimates of coeff ratio
+function bootstrap_coeffratio_ests(){
+    
+    PHENONAME=$1
+    VALIDATION=$2
+    METHOD=$3
+    DATASET=$4
+
+    if [[ $DATASET == "mcs" ]]; then
+        phenofile="/var/genetics/data/mcs/private/latest/raw/downloaded/NCDS_SFTP_1TB_1/imputed/phen/${PHENONAME}/pheno.pheno"
+        processed_dir="/var/genetics/data/mcs/private/latest/processed/pgs/fpgs/${PHENONAME}/${METHOD}"
+    elif [[ $DATASET == "ukb" ]]; then
+        phenofile="/var/genetics/data/ukb/private/latest/processed/proj/within_family/phen/${PHENONAME}/pheno.pheno"
+        processed_dir="/var/genetics/data/ukb/private/latest/processed/proj/within_family/pgs/fpgs/${PHENONAME}/${METHOD}"
+    fi
+
+    bootstrap_out="${within_family_path}/processed/fpgs/${PHENONAME}/${METHOD}/${DATASET}/${VALIDATION}/dirpop_coeffratiodiff"
+
+    python ${within_family_path}/scripts/fpgs/bootstrapest.py \
+        ${bootstrap_out} \
+        --pgsgroup1 ${processed_dir}/population_full.pgs.txt,${processed_dir}/population_proband.pgs.txt \
+        --pgsgroup2 ${processed_dir}/direct_full.pgs.txt,${processed_dir}/direct_proband.pgs.txt \
+        --phenofile ${phenofile} \
+        --pgsreg-r2
+
+}
+
+
+
 ## mcs outcomes
 for pheno in "ea" "cognition"
 do
     for validation in "ea" "cognition" "cogverb"
     do
-        run_fpgs_regression "${pheno}" "${validation}" "0" "prscs" "mcs" ""
+        # run_fpgs_regression "${pheno}" "${validation}" "0" "prscs" "mcs" ""
+        bootstrap_coeffratio_ests "${pheno}" "${validation}" "prscs" "mcs"
     done
 done
 
@@ -68,6 +100,7 @@ for pheno in "ea" "cognition"
 do
     for validation in "ea" "cognition"
     do
-        run_fpgs_regression "${pheno}" "${validation}" "0" "prscs" "ukb" ""
+        # run_fpgs_regression "${pheno}" "${validation}" "0" "prscs" "ukb" ""
+        bootstrap_coeffratio_ests "${pheno}" "${validation}" "prscs" "ukb"
     done
 done

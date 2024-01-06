b404239d61feab8356f7119510fb4cc30f6f1529
Tammy Tan <tammy.t133@gmail.com>
2024-01-05 21:28:06 -0500

    add median n filtering back

--- scripts/usingpackage/neuroticism/ldsc_analysis.sh
+++ scripts/usingpackage/neuroticism/ldsc_analysis.sh
@@ -12,14 +12,14 @@ source /disk/genetics/pub/python_env/anaconda2/bin/activate /disk/genetics/pub/p
 
 echo "Munging!!"
 ${ldscpath}/munge_sumstats.py \
---sumstats ${within_family_path}/processed/package_output/neuroticism/meta.sumstats.gz \
+--sumstats ${within_family_path}/processed/package_output/neuroticism/meta.nfilter.sumstats.gz \
 --out ${within_family_path}/processed/package_output/neuroticism/populationmunged \
 --N-col population_N --p population_pval --signed-sumstats population_z,0 \
 --merge-alleles ${hm3snps} \
 --n-min 1.0
 
 ${ldscpath}/munge_sumstats.py \
---sumstats ${within_family_path}/processed/package_output/neuroticism/meta.sumstats.gz \
+--sumstats ${within_family_path}/processed/package_output/neuroticism/meta.nfilter.sumstats.gz \
 --out ${within_family_path}/processed/package_output/neuroticism/directmunged \
 --N-col direct_N --p direct_pval --signed-sumstats direct_z,0 \
 --merge-alleles ${hm3snps} \
@@ -61,6 +61,6 @@ ${ldscpath}/ldsc.py \
 
 # Changing env
 source /var/genetics/proj/within_family/snipar_venv/bin/activate
-/var/genetics/proj/within_family/snipar_simulate/snipar/scripts/correlate.py /var/genetics/proj/within_family/within_family_project/processed/package_output/neuroticism/meta \
+/var/genetics/proj/within_family/snipar_simulate/snipar/scripts/correlate.py /var/genetics/proj/within_family/within_family_project/processed/package_output/neuroticism/meta.nfilter \
 /var/genetics/proj/within_family/within_family_project/processed/package_output/neuroticism/marginal \
 --ldscores /disk/genetics/ukb/jguan/ukb_analysis/output/ldsc/v2/@

46c58ab75d603d198b80440d484fe405639338e0
Tammy Tan <tammy.t133@gmail.com>
2024-02-19 00:12:49 -0500

    record log file

--- scripts/genomic_sem/cross_trait_genomic_sem.R
+++ scripts/genomic_sem/cross_trait_genomic_sem.R
@@ -100,9 +100,16 @@ source("/var/genetics/proj/within_family/within_family_project/scripts/genomic_s
 ldsc <- "/var/genetics/pub/data/ld_ref_panel/eur_w_ld_chr/"
 ss_basepath <- "/var/genetics/proj/within_family/within_family_project/processed/package_output/"
 
+# phenotypes <- c("aafb", "adhd", "agemenarche", "asthma", "aud", "bmi", "bpd", "bps", "cannabis", "cognition", "copd", "cpd", "depression",
+#                  "depsymp", "dpw", "ea", "eczema", "eversmoker", "extraversion", "fev", "hayfever", "hdl", "health", "height", "hhincome", "hypertension", "income", 
+#                  "migraine", "morningperson", "nchildren", "nearsight", "neuroticism", "nonhdl", "swb")
+
+# without extraversion
 phenotypes <- c("aafb", "adhd", "agemenarche", "asthma", "aud", "bmi", "bpd", "bps", "cannabis", "cognition", "copd", "cpd", "depression",
-                 "depsymp", "dpw", "ea", "eczema", "eversmoker", "extraversion", "fev", "hayfever", "hdl", "health", "height", "hhincome", "hypertension", "income", 
+                 "depsymp", "dpw", "ea", "eczema", "eversmoker", "fev", "hayfever", "hdl", "health", "height", "hhincome", "hypertension", "income", 
                  "migraine", "morningperson", "nchildren", "nearsight", "neuroticism", "nonhdl", "swb")
+
+sink("/var/genetics/proj/within_family/within_family_project/processed/genomic_sem/cross_trait/genomicSEM.log")
 
 for (pheno1 in phenotypes) {
     pheno1_index <- which(phenotypes == pheno1)
@@ -110,16 +117,18 @@ for (pheno1 in phenotypes) {
         pheno2_index <- which(phenotypes == pheno2)
         if (pheno2_index > pheno1_index) {
             run_cross_trait(pheno1 = pheno1,
-                            pheno2 = pheno2,
-                            pheno1_direct = paste0(ss_basepath, pheno1, "/direct.sumstats.gz"),
-                            pheno1_pop = paste0(ss_basepath, pheno1, "/population.sumstats.gz"),
-                            pheno2_direct = paste0(ss_basepath, pheno2, "/direct.sumstats.gz"),
-                            pheno2_pop = paste0(ss_basepath, pheno2, "/population.sumstats.gz"),
-                            ldsc = ldsc,
-                            analyze_results = TRUE)
+                        pheno2 = pheno2,
+                        pheno1_direct = paste0(ss_basepath, pheno1, "/direct.sumstats.gz"),
+                        pheno1_pop = paste0(ss_basepath, pheno1, "/population.sumstats.gz"),
+                        pheno2_direct = paste0(ss_basepath, pheno2, "/direct.sumstats.gz"),
+                        pheno2_pop = paste0(ss_basepath, pheno2, "/population.sumstats.gz"),
+                        ldsc = ldsc,
+                        analyze_results = TRUE)
         }
     }
 }
+
+sink()
 
 ## compile results
 compile_results(phenotypes)

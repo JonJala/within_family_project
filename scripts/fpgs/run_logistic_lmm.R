84251f8b4d0083dd9b34d942d668b49f3bfd945a
Tammy Tan <tammy.t133@gmail.com>
2024-04-18 03:51:32 -0400

    automate

--- scripts/fpgs/run_logistic_lmm.R
+++ scripts/fpgs/run_logistic_lmm.R
@@ -72,19 +72,52 @@ p <- p %>%
       V19 = (V19-mean(V19))/sd(V19),
       V20 = (V20-mean(V20))/sd(V20))
 
-## 1 gen logistic linear mixed model fit
-# glmm = glmer(phenotype ~ proband+(1|FID)+age+sex+agesex+V1+V2,data=p,family=binomial(link='logit'),nAGQ=0,glmerControl(optimizer="bobyqa"))
-glmm = glm(phenotype ~ proband+age+sex+agesex+V1+V2+V3+V4+V5+V6+V7+V8+V9+V10+V11+V12+V13+V14+V15+V16+V17+V18+V19+V20,data=p,family=binomial(link='logit'))
-write.table(summary(glmm)$coefficients,paste0(outpath,'.1.effects.txt'),quote=F)
-write.table(as.matrix(vcov(glmm)),paste0(outpath,'.1.vcov.txt'),quote=F) 
+if (phenotype == "eczema") {
 
-## 2 gen logistic linear mixed model fit
-glmm = glm(phenotype ~ proband+paternal+maternal+age+sex+agesex+V1+V2+V3+V4+V5+V6+V7+V8+V9+V10+V11+V12+V13+V14+V15+V16+V17+V18+V19+V20,data=p,family=binomial(link='logit'))
+  ## 1 gen OLS (glm and glmm had convergence issues)
+  glmm = lm(phenotype ~ proband+age+sex+agesex+V1+V2+V3+V4+V5+V6+V7+V8+V9+V10+V11+V12+V13+V14+V15+V16+V17+V18+V19+V20,data=p)
+  write.table(summary(glmm)$coefficients,paste0(outpath,'.1.effects.txt'),quote=F)
+  write.table(as.matrix(vcov(glmm)),paste0(outpath,'.1.vcov.txt'),quote=F) 
 
-# g2 = try({glmm = glmer(phenotype ~ proband+paternal+maternal+(1|FID)+age+sex+agesex+V1+V2,data=p,family=binomial(link='logit'),nAGQ=0,glmerControl(optimizer="bobyqa"))})
-# if (class(g2)=='try-error'){
-#     print('2 gen failed')
-#     glmm = glm(phenotype ~ proband+paternal+maternal+age+sex+agesex+V1+V2+V3+V4+V5,data=p,family=binomial(link='logit'))
-# }
-write.table(summary(glmm)$coefficients,paste0(outpath,'.2.effects.txt'),quote=F)
-write.table(as.matrix(vcov(glmm)),paste0(outpath,'.2.vcov.txt'),quote=F)
+  ## 2 gen OLS
+  glmm = lm(phenotype ~ proband+paternal+maternal+age+sex+agesex+V1+V2+V3+V4+V5+V6+V7+V8+V9+V10+V11+V12+V13+V14+V15+V16+V17+V18+V19+V20,data=p)
+  write.table(summary(glmm)$coefficients,paste0(outpath,'.2.effects.txt'),quote=F)
+  write.table(as.matrix(vcov(glmm)),paste0(outpath,'.2.vcov.txt'),quote=F)
+
+} else if (phenotype == "migraine") {
+  
+  ## 1 gen logistic linear mixed model fit with 5 PCs and NAGQ = 0
+  glmm = glmer(phenotype ~ proband+(1|FID)+age+sex+agesex+V1+V2+V3+V4+V5,data=p,family=binomial(link='logit'),nAGQ=0,glmerControl(optimizer="bobyqa"))
+  write.table(summary(glmm)$coefficients,paste0(outpath,'.1.effects.txt'),quote=F)
+  write.table(as.matrix(vcov(glmm)),paste0(outpath,'.1.vcov.txt'),quote=F) 
+
+  ## 2 gen logistic linear mixed model fit with 5 PCs and NAGQ = 0
+  glmm = glmer(phenotype ~ proband+paternal+maternal+(1|FID)+age+sex+agesex+V1+V2+V3+V4+V5,data=p,family=binomial(link='logit'),nAGQ=0,glmerControl(optimizer="bobyqa"))
+  write.table(summary(glmm)$coefficients,paste0(outpath,'.2.effects.txt'),quote=F)
+  write.table(as.matrix(vcov(glmm)),paste0(outpath,'.2.vcov.txt'),quote=F)
+
+} else if (phenotype == "depression") {
+
+  ## 1 gen GLM
+  glmm = glm(phenotype ~ proband+age+sex+agesex+V1+V2+V3+V4+V5+V6+V7+V8+V9+V10+V11+V12+V13+V14+V15+V16+V17+V18+V19+V20,data=p,family=binomial(link='logit'))
+  write.table(summary(glmm)$coefficients,paste0(outpath,'.1.effects.txt'),quote=F)
+  write.table(as.matrix(vcov(glmm)),paste0(outpath,'.1.vcov.txt'),quote=F) 
+
+  ## 2 gen GLM
+  glmm = glm(phenotype ~ proband+paternal+maternal+age+sex+agesex+V1+V2+V3+V4+V5+V6+V7+V8+V9+V10+V11+V12+V13+V14+V15+V16+V17+V18+V19+V20,data=p,family=binomial(link='logit'))
+  write.table(summary(glmm)$coefficients,paste0(outpath,'.2.effects.txt'),quote=F)
+  write.table(as.matrix(vcov(glmm)),paste0(outpath,'.2.vcov.txt'),quote=F)
+
+} else {
+
+  ## 1 gen logistic linear mixed model fit
+  glmm = glmer(phenotype ~ proband+(1|FID)+age+sex+agesex+V1+V2+V3+V4+V5+V6+V7+V8+V9+V10,data=p,family=binomial(link='logit'),nAGQ=1,glmerControl(optimizer="bobyqa"))
+  write.table(summary(glmm)$coefficients,paste0(outpath,'.1.effects.txt'),quote=F)
+  write.table(as.matrix(vcov(glmm)),paste0(outpath,'.1.vcov.txt'),quote=F) 
+
+  ## 2 gen logistic linear mixed model fit
+  glmm = glmer(phenotype ~ proband+paternal+maternal+(1|FID)+age+sex+agesex+V1+V2+V3+V4+V5+V6+V7+V8+V9+V10,data=p,family=binomial(link='logit'),nAGQ=1,glmerControl(optimizer="bobyqa"))
+  write.table(summary(glmm)$coefficients,paste0(outpath,'.2.effects.txt'),quote=F)
+  write.table(as.matrix(vcov(glmm)),paste0(outpath,'.2.vcov.txt'),quote=F)
+
+}

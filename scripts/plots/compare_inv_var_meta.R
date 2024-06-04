7775bd73b142fcd5f6a5276d59dd8eff2d78b651
Tammy Tan <tammy.t133@gmail.com>
2024-06-03 20:23:44 -0400

    change labels, calculate correlation

--- scripts/plots/compare_inv_var_meta.R
+++ scripts/plots/compare_inv_var_meta.R
@@ -32,5 +32,8 @@ p = merged %>%
         geom_abline(intercept=0, slope=1, linetype="solid", color="gray") +
         geom_hline(yintercept=0, linetype="dotted") +
         geom_vline(xintercept=0, linetype="dotted") +
-        labs(y = "Inverse Variance Weighted", x = "Regular Meta-Analysis")
+        labs(y = "Univariate Meta-Analysis", x = "Multivariate Meta-Analysis")
 ggsave("/var/genetics/proj/within_family/within_family_project/processed/figures/inv_var_meta_comparison.jpg", p)
+
+# calculate correlation
+cor(merged$direct_Beta, merged$inv_var_direct, use = "complete.obs") # 0.999

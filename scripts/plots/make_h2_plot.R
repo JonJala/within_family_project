09d3df5ab03d07917af904bf42960e8266a31951
Tammy Tan <tammy.t133@gmail.com>
2024-06-20 14:41:11 -0400

    change colour palette

--- scripts/plots/make_h2_plot.R
+++ scripts/plots/make_h2_plot.R
@@ -61,22 +61,19 @@ h2_plot <- function(save = TRUE) {
     # get colour palette
     set.seed(42)
     n <- length(unique(dat$phenotype))
-    palette1 <- c("#E93993", "#CCEBDE", "#B2BFDC", "#C46627", "#10258A", "#E73B3C", "#DDDDDD",
-                    "#FF8F1F", "#FCD3E6", "#B9E07A", "#A3F6FF", "#FFFF69", "#47AA42", "#A0DBD0",
-                    "#5791C2", "#E9B82D", "#FC9284", "#7850A4", "#CEB8D7", "#FDC998", "#ADADAD",
-                    "#00441B", "#028189", "#67001F", "#525252", "#FE69FC", "#A0D99B", "#4B1DB7")
-    qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
-    col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
-    palette2 <- sample(col_vector, n-length(palette1), replace = F)
+    palette1 <- c("#E93993", "#C46627", "#10258A", "#d71a1a", "#3cc035", 
+                    "#5791C2", "#ec5945", "#925bcd", "#00441B", "#028189", "#67001F", "#424242", "#ec16e8", "#4B1DB7",
+                    "#6e0e10", "#377EB8",  "#A65628", "#8bad55", "#FF7F00", "#672870", "#999999", "#9a4572")
+    palette2 <- sample(palette1, n-length(palette1), replace = T)
     palette <- c(palette1, palette2)
 
     # plot
     dat %>%
         ggplot() +
-        geom_point(aes(x = h2_population, y = h2_direct, colour=phenotype, shape=phenotype), alpha=0.6) +
+        geom_point(aes(x = h2_population, y = h2_direct, colour=phenotype, shape=phenotype)) +
         geom_label_repel(data = dat_points, aes(x = h2_population, y = h2_direct, label = phenotype), xlim = c(0.18, NA), box.padding = 1.2) +
-        geom_linerange(aes(x=h2_population, ymin = h2_direct-h2_se_direct, ymax=h2_direct+h2_se_direct, color = phenotype), alpha=0.6) +
-        geom_linerange(aes(y=h2_direct, xmin = h2_population-h2_se_population, xmax=h2_population+h2_se_population, color = phenotype), alpha=0.6) +
+        geom_linerange(aes(x=h2_population, ymin = h2_direct-h2_se_direct, ymax=h2_direct+h2_se_direct, color = phenotype)) +
+        geom_linerange(aes(y=h2_direct, xmin = h2_population-h2_se_population, xmax=h2_population+h2_se_population, color = phenotype)) +
         geom_abline(intercept=0, slope=1, linetype="solid", color="gray") +
         geom_hline(yintercept=0, linetype="dotted", color="black") +
         geom_vline(xintercept=0, linetype="dotted", color="black") +

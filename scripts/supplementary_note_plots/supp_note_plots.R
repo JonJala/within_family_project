88e4a8f637d3981ed78f0b9451557895a2d4c2e4
Tammy Tan <tammy.t133@gmail.com>
2024-06-20 14:42:45 -0400

    updated sef plot script

---
+++ scripts/supplementary_note_plots/supp_note_plots.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+
+# ---------------------------------------------------------------------
+# description: create figures for supplementary note
+# ---------------------------------------------------------------------
+
+list.of.packages <- c("data.table", "tidyr", "dplyr", "magrittr", "tidyverse", "ggplot2")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.rstudio.com/")
+lapply(list.of.packages, library, character.only = TRUE)
+
+# ---------------------------------------------------------------------
+# SEF figure
+# ---------------------------------------------------------------------
+
+data <- fread("/disk/genetics/proj/within_family/within_family_project/processed/qc/fhs/bmi/CLEANED.out.gz")
+
+data %<>% 
+    mutate(sef_x = sqrt(f*(1-f)))
+
+p <- data %>%
+        ggplot(aes(x = sef_x, y = se_direct)) +
+        geom_point() +
+        xlab(bquote(sqrt("f(1-f)"))) +
+        ylab("Standard Error of Direct Genetic Effects") +
+        theme_classic() +
+        theme(panel.grid.major = element_line(color = "grey",
+                                        linewidth = 0.5,
+                                        linetype = 2))
+ggsave("/var/genetics/proj/within_family/within_family_project/doc/exampleplots/fhs_bmi_sef.pdf", p)
+
+
+# ---------------------------------------------------------------------
+# sampling correlation figure
+# ---------------------------------------------------------------------
+

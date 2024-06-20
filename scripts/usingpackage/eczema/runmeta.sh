2b7c8f27fb47ea5acbd538c98a466af864ab7d87
Tammy Tan <tammy.t133@gmail.com>
2024-06-20 14:53:57 -0400

    uncomment

--- scripts/usingpackage/eczema/runmeta.sh
+++ scripts/usingpackage/eczema/runmeta.sh
@@ -2,10 +2,10 @@
 
 scriptpath="/var/genetics/proj/within_family/within_family_project/scripts/package"
 
-# ## with UKB
-# python $scriptpath/run_metaanalysis.py \
-# "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/eczema/inputfiles.json" \
-# --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/eczema/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/eczema/meta.log
+## with UKB
+python $scriptpath/run_metaanalysis.py \
+"/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/eczema/inputfiles.json" \
+--outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/eczema/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/eczema/meta.log
 
 ## without UKB
 python $scriptpath/run_metaanalysis.py \

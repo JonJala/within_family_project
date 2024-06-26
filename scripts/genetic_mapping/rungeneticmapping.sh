990b6c19e3fbcf71d67c11b25550f447bb6ad59f
Tammy Tan <tammy.t133@gmail.com>
2024-06-26 11:28:16 -0400

    remove old folders and scripts

--- scripts/genetic_mapping/rungeneticmapping.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-python /var/genetics/proj/within_family/within_family_project/scripts/genetic_mapping/makegeneticmap.py \
-    "/var/genetics/proj/within_family/snipar_simulate/snipar/util_data/decode_map/chr_~.gz" \
-    --sumstat "/var/genetics/proj/within_family/within_family_project/processed/package_output/ea/meta.sumstats.gz" \
-    --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/ea/"

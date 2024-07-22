9a8976bab5e04fbbcf398186f99371f48d8066d7
Tammy Tan <tammy.t133@gmail.com>
2024-07-22 13:38:43 -0400

    uncomment

--- scripts/usingpackage/bmi/runmeta.sh
+++ scripts/usingpackage/bmi/runmeta.sh
@@ -3,9 +3,9 @@
 scriptpath="/var/genetics/proj/within_family/within_family_project/scripts/package"
 
 
-# python $scriptpath/run_metaanalysis.py \
-# "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/bmi/inputfiles.json" \
-# --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/bmi/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/bmi/meta.log
+python $scriptpath/run_metaanalysis.py \
+"/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/bmi/inputfiles.json" \
+--outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/bmi/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/bmi/meta.log
 
 # without hunt
 python $scriptpath/run_metaanalysis.py \
--- scripts/usingpackage/height/runmeta.sh
+++ scripts/usingpackage/height/runmeta.sh
@@ -2,15 +2,15 @@
 
 scriptpath="/var/genetics/proj/within_family/within_family_project/scripts/package"
 
-# ## without ukb
-# python $scriptpath/run_metaanalysis.py \
-# "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/height/inputfiles_noukb.json" \
-# --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/height/meta_noukb" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/height/meta_noukb.log
+## without ukb
+python $scriptpath/run_metaanalysis.py \
+"/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/height/inputfiles_noukb.json" \
+--outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/height/meta_noukb" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/height/meta_noukb.log
 
-# ## with ukb
-# python $scriptpath/run_metaanalysis.py \
-# "/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/height/inputfiles.json" \
-# --outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/height/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/height/meta.log
+## with ukb
+python $scriptpath/run_metaanalysis.py \
+"/var/genetics/proj/within_family/within_family_project/scripts/usingpackage/height/inputfiles.json" \
+--outprefix "/var/genetics/proj/within_family/within_family_project/processed/package_output/height/meta" | tee /var/genetics/proj/within_family/within_family_project/processed/package_output/height/meta.log
 
 ## without hunt
 python $scriptpath/run_metaanalysis.py \
